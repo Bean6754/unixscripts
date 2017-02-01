068760834247c271b27f15d8f89a4a33793556fe
Matthew <Bean6754@users.noreply.github.com>
2017-02-01 22:38:13 +0000

    Update GentooDesktopSetup.sh

--- GentooDesktopSetup.sh
+++ GentooDesktopSetup.sh
@@ -1,4 +1,39 @@
+## DO NOT USE YET!
+
 #!/bin/bash
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# make.conf stuff.
+echo 'CFLAGS="-march=native -O2 -pipe"' > /etc/portage/make.conf
+echo 'CXXFLAGS="${CFLAGS}"' >> /etc/portage/make.conf
+echo 'CHOST="x86_64-pc-linux-gnu"' >> /etc/portage/make.conf
+echo 'USE="gtk qt4 qt5 -kde -gnome dvd alsa cdr"' >> /etc/portage/make.conf
+echo 'CPU_FLAGS_X86="mmx sse sse2"' >> /etc/portage/make.conf
+echo 'PORTDIR="/usr/portage"' >> /etc/portage/make.conf
+echo 'DISTDIR="${PORTDIR}/distfiles"' >> /etc/portage/make.conf
+echo 'PKGDIR="${PORTDIR}/packages"' >> /etc/portage/make.conf
+echo 'MAKEOPTS="-j4"' >> /etc/portage/make.conf
+echo 'GENTOO_MIRRORS="ftp://mirror.bytemark.co.uk/gentoo/ http://mirror.bytemark.co.uk/gentoo/ rsync://mirror.bytemark.co.uk/gentoo/"' >> /etc/portage/make.conf
+
+eselect profile set 1
+emerge --update --deep --newuse @world
+
+rm -rf /etc/timezone
+ln -s /usr/share/zoneinfo/Europe/London > /etc/timezone
+emerge --config sys-libs/timezone-data
+echo 'en_GB.UTF-8 UTF-8' > /etc/locale.gen
+locale-gen
+eselect locale set 3
+env-update && source /etc/profile
 
 emerge sudo gcc
 
