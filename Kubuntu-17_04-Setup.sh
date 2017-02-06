716933461ed47f1659c7d1500a3c8e5566c2e40c
Matthew <Bean6754@users.noreply.github.com>
2017-02-06 00:27:52 +0000

    Update Kubuntu-17_04-Setup.sh

--- Kubuntu-17_04-Setup.sh
+++ Kubuntu-17_04-Setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 apt update -y
 apt upgrade -y
 apt dist-upgrade -y
