4b21d1364d294b41c5f99d8118e1fe935d8af2ce
Matthew <Bean6754@users.noreply.github.com>
2017-02-17 22:47:29 +0000

    Create Fedora-Desktop.sh

---
+++ Fedora-Desktop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# RPM Fusion: 
+dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+# Negativo1 multimedia: 
+dnf config-manager --add-repo=http://negativo17.org/repos/fedora-multimedia.repo
+
+dnf update -y 
+dnf install -y ruby gem gcc gcc-c++ python perl java-1.8.0-openjdk java-1.8.0-openjdk-devel gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-free-extras ffmpeg vlc libdvdnav libdvdread libdvdcss steam libreoffice gimp emacs-nox
+
+exit 0
