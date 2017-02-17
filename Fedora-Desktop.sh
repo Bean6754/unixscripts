3c9905a62a3580333884d2c221e43de03c421e57
Matthew <Bean6754@users.noreply.github.com>
2017-02-17 22:52:41 +0000

    Update Fedora-Desktop.sh

--- Fedora-Desktop.sh
+++ Fedora-Desktop.sh
@@ -16,6 +16,6 @@ dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-releas
 dnf config-manager --add-repo=http://negativo17.org/repos/fedora-multimedia.repo -y
 
 dnf update -y 
-dnf install -y ruby gem gcc gcc-c+ python perl java-1.8.0-openjdk java-1.8.0-openjdk-devel gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-free-extras ffmpeg vlc libdvdnav libdvdread libdvdcss steam libreoffice gimp emacs-nox qt5
+dnf install -y ruby gem gcc gcc-c+ python perl java-1.8.0-openjdk java-1.8.0-openjdk-devel gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-free-extras ffmpeg vlc libdvdnav libdvdread libdvdcss steam libreoffice gimp emacs-nox firefox qt5 gtk2 gtk3 glade
 
 exit 0
