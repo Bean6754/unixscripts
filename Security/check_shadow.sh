3249ee3bd5f1f6e93212fd9531ef59067153c140
Matthew <Bean6754@users.noreply.github.com>
2017-02-17 13:39:08 +0000

    Create check_shadow.sh

---
+++ Security/check_shadow.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+# ...
+
+grep "ENCRYPT_METHOD" /etc/login.defs
+
+exit 0
