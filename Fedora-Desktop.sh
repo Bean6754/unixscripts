3bea3bbbda6516631e8d69fc86baf36a9a4b6a5d
Matthew <Bean6754@users.noreply.github.com>
2017-02-17 22:48:04 +0000

    Update Fedora-Desktop.sh

--- Fedora-Desktop.sh
+++ Fedora-Desktop.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 # RPM Fusion: 
-dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 # Negativo1 multimedia: 
-dnf config-manager --add-repo=http://negativo17.org/repos/fedora-multimedia.repo
+dnf config-manager --add-repo=http://negativo17.org/repos/fedora-multimedia.repo -y
 
 dnf update -y 
 dnf install -y ruby gem gcc gcc-c++ python perl java-1.8.0-openjdk java-1.8.0-openjdk-devel gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-free-extras ffmpeg vlc libdvdnav libdvdread libdvdcss steam libreoffice gimp emacs-nox
