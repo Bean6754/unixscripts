89372f8946d1159dc30d52e4c5f347b3771c1b15
Matthew <Bean6754@users.noreply.github.com>
2017-02-23 15:59:13 +0000

    Update serverhardening.sh

--- serverhardening.sh
+++ serverhardening.sh
@@ -4,18 +4,20 @@
 
 # iptables and ssh server hardening
 
-echo "This script would not be made possible if it weren't for the article over at "https://javapipe.com/iptables-ddos-protection".
+echo "This script would not be made possible if it weren't for the article over at 'https://javapipe.com/iptables-ddos-protection'.
 
-echo " 1: Drop invalid packets "
 /sbin/iptables -t mangle -A PREROUTING -m conntrack --ctstate INVALID -j DROP
+echo "Added rule: 1. Drop invalid packets."
+echo
 
-echo " 2: Drop TCP packets that are new and are not SYN "
 /sbin/iptables -t mangle -A PREROUTING -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
+echo "Added rule: 2. Drop TCP packets that are new and are not SYN."
+echo
 
-echo " 3: Drop SYN packets with suspicious MSS value "
 /sbin/iptables -t mangle -A PREROUTING -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -j DROP
+echo "Added rule: 3. Drop SYN packets with suspicious MSS value."
+echo
 
-echo " 4: Block packets with bogus TCP flags "
 /sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP
 /sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
 /sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
@@ -30,8 +32,9 @@ echo " 4: Block packets with bogus TCP flags "
 /sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP
 /sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP
 /sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
+echo "Added rule: 4. Block packets with bogus TCP flags."
+echo
 
-echo " 5: Block spoofed packets "
 /sbin/iptables -t mangle -A PREROUTING -s 224.0.0.0/3 -j DROP
 /sbin/iptables -t mangle -A PREROUTING -s 169.254.0.0/16 -j DROP
 /sbin/iptables -t mangle -A PREROUTING -s 172.16.0.0/12 -j DROP
@@ -41,37 +44,47 @@ echo " 5: Block spoofed packets "
 /sbin/iptables -t mangle -A PREROUTING -s 0.0.0.0/8 -j DROP
 /sbin/iptables -t mangle -A PREROUTING -s 240.0.0.0/5 -j DROP
 /sbin/iptables -t mangle -A PREROUTING -s 127.0.0.0/8 ! -i lo -j DROP
+echo "Added rule: 5. Block spoofed packets."
+echo
 
-echo " 6: Drop ICMP (you usually don't need this protocol) "
 /sbin/iptables -t mangle -A PREROUTING -p icmp -j DROP
+echo "Added rule: 6. Drop ICMP. You usually do not need this protocol."
+echo
 
-echo " 7: Drop fragments in all chains "
 /sbin/iptables -t mangle -A PREROUTING -f -j DROP
+echo "Added rule: 7. Drop fragments in all chains."
+echo
 
-echo " 8: Limit connections per source IP "
 /sbin/iptables -A INPUT -p tcp -m connlimit --connlimit-above 111 -j REJECT --reject-with tcp-reset
+echo "Added rule: 8. Limit connections per source IP."
+echo
 
-echo " 9: Limit RST packets "
 /sbin/iptables -A INPUT -p tcp --tcp-flags RST RST -m limit --limit 2/s --limit-burst 2 -j ACCEPT
 /sbin/iptables -A INPUT -p tcp --tcp-flags RST RST -j DROP
+echo "Added rule: 9. Limit RST packets."
+echo
 
-echo " 10: Limit new TCP connections per second per source IP "
 /sbin/iptables -A INPUT -p tcp -m conntrack --ctstate NEW -m limit --limit 60/s --limit-burst 20 -j ACCEPT
 /sbin/iptables -A INPUT -p tcp -m conntrack --ctstate NEW -j DROP
+echo "Added rule: 10. Limit new TCP connections per second per source IP."
+echo
 
-echo " 11: Use SYNPROXY on all ports (disables connection limiting rule) "
 /sbin/iptables -t raw -A PREROUTING -p tcp -m tcp --syn -j CT --notrack
 /sbin/iptables -A INPUT -p tcp -m tcp -m conntrack --ctstate INVALID,UNTRACKED -j SYNPROXY --sack-perm --timestamp --wscale 7 --mss 1460
 /sbin/iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+echo "Added rule: 11. Use SYNPROXY on all ports. Disables connection limiting rule."
+echo
 
 # Extras.
-echo " SSH brute-force protection "
 /sbin/iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --set
 /sbin/iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 10 -j DROP
+echo "Added rule: Extra. SSH brute-force protection."
+echo
 
-echo " Protection against port scanning "
 /sbin/iptables -N port-scanning
 /sbin/iptables -A port-scanning -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s --limit-burst 2 -j RETURN
 /sbin/iptables -A port-scanning -j DROP
+echo "Added rule: Extra. Protection against port scanning."
+echo
 
 echo "Done!"
