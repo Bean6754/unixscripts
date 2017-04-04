872e29311f5d65a0a12df7bd65aff1425864a3ba
Matthew <Bean6754@users.noreply.github.com>
2017-04-04 22:40:47 +0100

    Create wifi.sh

---
+++ wifi.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+# ...
+
+# Disable and stop network services.
+systemctl disable NetworkManager
+systemctl stop NetworkManager
+systemctl disable netctl
+systemctl stop netctl
+systemctl disable wpa_supplicant
+systemctl stop wpa_supplicant
+
+# Set WiFi interface to down.
+ip link set wlo1 down
+# Start WiFi-Menu to do all of the WiFi for us.
+wifi-menu
+# Exit program sucessfully.
+exit 0
