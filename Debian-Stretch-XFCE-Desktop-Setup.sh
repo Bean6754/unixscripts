f6260d0bb2a266a4490761741018df3138154d68
Matthew <Bean6754@users.noreply.github.com>
2017-04-14 22:59:08 +0100

    Update Debian-Stretch-XFCE-Desktop-Setup.sh

--- Debian-Stretch-XFCE-Desktop-Setup.sh
+++ Debian-Stretch-XFCE-Desktop-Setup.sh
@@ -24,7 +24,7 @@ dpkg --add-architecture i386
 apt update -y
 apt upgrade -y
 apt dist-upgrade -y
-apt install -y *firmware* sudo ssh neovim git wget curl build-essential lua5.3 open-cobol gfortran-6 aptitude p7zip-full rar unrar unzip zip openjdk-8-jdk ffmpeg net-tools cifs-utils libdvdread4 libdvdnav4 libdvdcss2 transmission-gtk nmap steam gtk2-engines-murrine murrine-themes gstreamer1.0-plugins-bad gstreamer1.0-plugins-base gstreamer1.0-plugins-base-apps gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly qt5-default wireshark-gtk zenmap handbrake-cli handbreak 
+apt install -y *firmware* sudo ssh neovim git wget curl build-essential lua5.3 open-cobol gfortran-6 aptitude p7zip-full rar unrar unzip zip openjdk-8-jdk ffmpeg net-tools cifs-utils htop scrot libdvdread4 libdvdnav4 libdvdcss2 transmission-gtk nmap steam gtk2-engines-murrine murrine-themes gstreamer1.0-plugins-bad gstreamer1.0-plugins-base gstreamer1.0-plugins-base-apps gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly qt5-default wireshark-gtk zenmap handbrake-cli handbreak 
 # NOTE. You'll have to install nvidia drivers, intel microcode, amd64-microcode, etc.., yourself.
 dpkg-reconfigure libdvd-pkg
 
