c8fba48c57d162df104f6ebad02631dfa79691a9
Matthew <Bean6754@users.noreply.github.com>
2017-05-11 22:56:03 +0100

    Create CentOS7.3-XFCE-Desktop-Setup.sh

---
+++ CentOS7.3-XFCE-Desktop-Setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# Update system.
+yum update -y
+# Install stuff.
+yum install -y epel-release microcode_ctl kernel-devel zip unzip p7zip p7zip-plugins git wget curl htop libbluray firefox libreoffice firewall-config pulseaudio alsa-plugins-pulseaudio alsa-utils dvd+rw-tools pulseaudio-module-x11 pulseaudio-utils pavucontrol
+yum groupinstall -y "X Window system" "Xfce" "Development Tools"
+
+# Start GDM at boot.
+systemctl set-default graphical.target
+
+# Install neofetch and run it.
+git clone https://github.com/dylanaraps/neofetch
+cd neofetch
+make install
+cd ..
+rm -rf neofetch
+clear
+neofetch
+
+# Exit script.
+exit 0
