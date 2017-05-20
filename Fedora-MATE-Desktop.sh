375aaaccff02171e801dc2854d3f3ad1505199c1
Matthew <Bean6754@users.noreply.github.com>
2017-05-20 10:28:57 +0100

    Update and rename Fedora-Desktop.sh to Fedora-MATE-Desktop.sh

--- Fedora-MATE-Desktop.sh
+++ Fedora-MATE-Desktop.sh
@@ -14,8 +14,10 @@ fi
 dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 # Negativo1 multimedia: 
 dnf config-manager --add-repo=http://negativo17.org/repos/fedora-multimedia.repo -y
+#Negativo1 steam: 
+dnf config-manager --add-repo=http://negativo17.org/repos/fedora-steam.repo -y
 
 dnf update -y 
-dnf install -y @kde-desktop kdm git wget curl htop nmap ruby gem gcc gcc-c++ python perl java-1.8.0-openjdk java-1.8.0-openjdk-devel gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-free-extras ffmpeg vlc libdvdnav libdvdread libdvdcss steam libreoffice gimp vim firefox transmission-cli transmission-qt qt5 qt5-designer
+dnf install -y git wget curl htop nmap ruby gem gcc gcc-c++ python perl java-1.8.0-openjdk java-1.8.0-openjdk-devel gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-free-extras ffmpeg vlc libdvdnav libdvdread libdvdcss steam libreoffice gimp vim firefox transmission-cli transmission-gtk qt5 qt5-designer
 
 exit 0
