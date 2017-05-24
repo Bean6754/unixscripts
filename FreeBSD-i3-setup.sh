65fe164f95c02b595199731c9ec23da8de274ed7
Matthew <Bean6754@users.noreply.github.com>
2017-05-24 14:11:54 +0100

    Create FreeBSD-i3-setup.sh

---
+++ FreeBSD-i3-setup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+portsnap fetch
+portsnap extract
+portsnap update
+
+pkg update
+pkg upgrade -y
+
+pkg install -y vim sudo p7zip rar unrar zip unzip xorg xinit i3 i3status i3lock feh rofi xarchiver firefox vlc libdvdcss libdvdnav libdvdread
+
+echo 'proc /proc procfs rw,noauto 0 0' >> /etc/fstab
+
+echo 'linux_enable="YES"' >> /etc/rc.conf
+echo 'dbus_enable="YES"' >> /etc/rc.conf
+echo 'hald_enable="YES"' >> /etc/rc.conf
+
+# NVIDIA-Driver Stuff.
+# pkg install -y nvidia-driver nvidia-settings nvidia-xconfig nvidia-texture-tools
+# echo 'nvidia_load="YES"' >> /boot/loader.conf
+# echo 'nvidia_name="nvidia"' >> /boot/loader.conf
+# echo 'nvidia_modeset_load="YES"' >> /boot/loader.conf
+# echo 'nvidia_modeset_name="nvidia-modeset"' >> /boot/loader.conf
+# echo 'kld_list="nvidia nvidia-modeset"' >> /etc/rc.conf
+# echo 'nvidia_name="nvidia"' >> /etc/rc.conf
+# echo 'nvidia_modeset_name="nvidia-modeset"' >> /etc/rc.conf
