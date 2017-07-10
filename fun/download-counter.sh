34e7818866d283cc360ec5d5fd6015e865d28549
Matthew <Bean6754@users.noreply.github.com>
2017-07-10 07:48:20 -0500

    Create download-counter.sh

---
+++ fun/download-counter.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+while true
+do
+  clear
+	filesize=$(stat -c%s /mnt/Win10_1703_English_x64.iso)
+	expr "$filesize" / 1000000 && echo MB
+	echo
+	expr "$filesize" / 1000000000 && echo GB
+	# wc -c /mnt/Win10_1703_English_x64.iso | awk '{print $1}'
+	sleep 6
+done
+
+exit 0
