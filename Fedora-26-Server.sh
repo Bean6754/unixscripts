3ed3f6ddc6a1bcc75a0a2936ff1ef109e3cab16c
Matthew <Bean6754@users.noreply.github.com>
2017-08-11 17:59:54 +0100

    Update Fedora-26-Server.sh

--- Fedora-26-Server.sh
+++ Fedora-26-Server.sh
@@ -12,8 +12,8 @@ fi
 
 ## Repositories.
 
-# RPM Fusion: 
-dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+# RPM Fusion:   (Optional, not needed.)
+# dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 ## Packages.
 
