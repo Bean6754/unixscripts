1550ec8d6d9c861021aef334a02616e0528bf6f2
Matthew <Bean6754@users.noreply.github.com>
2017-08-30 19:29:59 +0100

    Update Fedora-26-Server.sh

--- Fedora-26-Server.sh
+++ Fedora-26-Server.sh
@@ -14,8 +14,9 @@ fi
 
 # RPM Fusion:   (Optional, not needed.)
 # dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-# Negativo17 multimedia: (Needed for rar and unrar.)
-dnf config-manager --add-repo=http://negativo17.org/repos/fedora-multimedia.repo -y
+# Negativo17 multimedia: (Needed for rar and unrar.)  NOTE: I do not use rar anymore. That's why it's commented out.
+#dnf config-manager --add-repo=http://negativo17.org/repos/fedora-multimedia.repo -y
+#dnf install -y rar unrar
 
 ## Packages.
 
@@ -24,7 +25,7 @@ dnf update -y
 # Group packages.
 dnf groupinstall -y development-tools
 # Singular packages.
-dnf install -y git wget curl htop nmap ruby gem gcc gcc-c++ python perl java-1.8.0-openjdk java-1.8.0-openjdk-devel vim transmission-cli iftop p7zip rar unrar zip unzip
+dnf install -y git wget curl htop nmap ruby gem gcc gcc-c++ python perl java-1.8.0-openjdk java-1.8.0-openjdk-devel vim transmission-cli iftop p7zip zip unzip
 
 # Enable firewalld at boot (and start it).
 systemctl enable firewalld && systemctl start firewalld
