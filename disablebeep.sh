993d9a1431b1041fee0167567e351d4fc2eddfb1
Matthew <Bean6754@users.noreply.github.com>
2017-09-15 14:56:23 +0100

    Create disablebeep.sh

---
+++ disablebeep.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# Disable PC Speaker now and at bootup.
+rmmod pcspkr
+mkdir -p /etc/modprobe.d
+touch /etc/modprobe.d/pcspkr.conf
+echo 'blacklist pcspkr' > /etc/modprobe.d/pcspkr.conf
+
+exit 0
