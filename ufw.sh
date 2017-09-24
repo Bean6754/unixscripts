faa71a2e81f241ee0e7821130ef9a0728f11baed
Matthew <Bean6754@users.noreply.github.com>
2017-09-24 16:31:51 +0100

    Create ufw.sh

---
+++ ufw.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# Enable then start service at boot.
+systemctl enable ufw
+systemctl start ufw
+
+# Enable firewall.
+ufw enable
+
+# SSH.
+ufw allow 22/tcp
+# HTTP.
+ufw allow 80/tcp
+# HTTPS.
+ufw allow 443/tcp
+
+# Reload firewall.
+ufw reload
+
+exit 0
