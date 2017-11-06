d4386bb751f5c5cc1a52a8863fa455ba6f6065ef
Matthew <Bean6754@users.noreply.github.com>
2017-11-06 12:31:12 +0000

    Update CentOS-7-XFCE-Desktop-Setup.sh

--- CentOS-7-XFCE-Desktop-Setup.sh
+++ CentOS-7-XFCE-Desktop-Setup.sh
@@ -16,10 +16,20 @@ yum update -y
 yum install -y epel-release
 yum localinstall --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-7.noarch.rpm
 yum update -y
-yum install -y dnf microcode_ctl kernel-devel zip unzip p7zip p7zip-plugins git wget curl htop strace lof nc tcpdump net-tools libbluray firefox libreoffice firewall-config pulseaudio alsa-plugins-pulseaudio alsa-utils dvd+rw-tools pulseaudio-module-x11 pulseaudio-utils pavucontrol xarchiver mousepad gimp vlc vlc-extras qt5-qtbase qt5-qtconfiguration transmission transmission-cli wireshark wireshark-gnome
+yum install -y dnf microcode_ctl kernel-devel dkms zip unzip p7zip p7zip-plugins git wget curl htop strace lof nc tcpdump net-tools libbluray firefox libreoffice firewall-config pulseaudio alsa-plugins-pulseaudio alsa-utils dvd+rw-tools pulseaudio-module-x11 pulseaudio-utils pavucontrol xarchiver mousepad gimp vlc vlc-extras qt5-qtbase qt5-qtconfiguration transmission transmission-cli wireshark wireshark-gnome
 yum groupinstall -y "X Window system" "Xfce" "Development Tools"
 # XFCE4 extras.
 yum install -y xfce4-*-plugin xfce4-about xfce4-vala
+
+# Steam.
+# Dependancies.
+# yum install mesa-libGL mesa-libGLES mesa-libGLU mesa-libGLw mesa-libEGL mesa-libGL.i686 mesa-libGLES.i686 mesa-libGLU.i686 mesa-libGLw.i686 mesa-libEGL.i686
+# wget http://repo.steampowered.com/steam/archive/precise/steam_latest.tar.gz
+# tar xvf steam_latest.tar.gz
+# cd steam
+# make install
+# cd ..
+# rm -rf steam
 
 # Start GDM or Lightdm at boot.
 systemctl set-default graphical.target
