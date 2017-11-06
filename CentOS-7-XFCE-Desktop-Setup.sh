78bc05b860626d0c01e6ff5d4145caa48dd2c947
Matthew <Bean6754@users.noreply.github.com>
2017-11-06 11:56:01 +0000

    Update CentOS-7-XFCE-Desktop-Setup.sh

--- CentOS-7-XFCE-Desktop-Setup.sh
+++ CentOS-7-XFCE-Desktop-Setup.sh
@@ -14,8 +14,9 @@ fi
 yum update -y
 # Install stuff.
 yum install -y epel-release
+yum localinstall --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-7.noarch.rpm
 yum update -y
-yum install -y dnf microcode_ctl kernel-devel zip unzip p7zip p7zip-plugins git wget curl htop strace lsof nc tcpdump net-tools libbluray firefox libreoffice firewall-config pulseaudio alsa-plugins-pulseaudio alsa-utils dvd+rw-tools pulseaudio-module-x11 pulseaudio-utils pavucontrol
+yum install -y dnf microcode_ctl kernel-devel zip unzip p7zip p7zip-plugins git wget curl htop strace lsof nc tcpdump net-tools libbluray firefox libreoffice firewall-config pulseaudio alsa-plugins-pulseaudio alsa-utils dvd+rw-tools pulseaudio-module-x11 pulseaudio-utils pavucontrol xarchiver vlc vlc-extras
 yum groupinstall -y "X Window system" "Xfce" "Development Tools"
 # XFCE4 extras.
 yum install -y xfce4-*-plugin xfce4-about xfce4-vala
