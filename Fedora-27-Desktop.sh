587d2b288a81ba8fdfae6ab589f1cb9465c47865
Matthew <Bean6754@users.noreply.github.com>
2017-11-07 00:17:29 +0000

    Create Fedora-27-Desktop.sh

---
+++ Fedora-27-Desktop.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# Repos.
+# RPMFusion.
+dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+# Update repositories and any potential packages.
+dnf update -y
+# Group packages.
+dnf groupinstall -y "Development-Tools" "Security-Lab"
+# Low level.
+dnf install -y kernel-devel kernel-headers acpid dkms strace lsof htop git curl wget vim transmission-cli gcc-c++ ruby nmap ibdvdnav libdvdread libbluray p7zip p7zip-plugins zip unzip
+# High level.
+dnf install -y transmission vlc vlc-extras pavucontrol
+# Adobe Flash.
+rpm -ivh http://linuxdownload.adobe.com/adobe-release/adobe-release-x86_64-1.0-1.noarch.rpm
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
+dnf install flash-plugin alsa-plugins-pulseaudio libcurl
+
+exit 0
