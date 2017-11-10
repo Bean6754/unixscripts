cae19bcd6930ccaaee9bb673f1123d7a78c67127
Matthew <Bean6754@users.noreply.github.com>
2017-11-10 21:21:40 +0000

    Update Debian-Buster-KDE.sh

--- Debian-Buster-KDE.sh
+++ Debian-Buster-KDE.sh
@@ -21,8 +21,10 @@ apt install -y aptitude git curl wget vim sudo fakeroot p7zip-full zip unzip str
 # High-level
 apt install -y wireshark qbittorrent gimp vlc libdvdnav4 libdvdread4 libdvdcss2 libbluray2
 dpkg-reconfigure libdvd-pkg
+# Flash player.
+apt install -y browser-plugin-freshplayer-pepperflash
 # Qt development.
-apt install -y qt5-default qt4-default qtdeclarative5-dev
+apt install -y qt5-default qtdeclarative5-dev
 # Java.
 apt purge -y openjdk*
 apt autoremove -y
