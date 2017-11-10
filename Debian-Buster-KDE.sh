e7f069f0342b8622f30257ebd9c98d0096d3e80a
Matthew <Bean6754@users.noreply.github.com>
2017-11-10 17:30:41 +0000

    Update Debian-Buster-KDE.sh

--- Debian-Buster-KDE.sh
+++ Debian-Buster-KDE.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 ## Make sure free, non-free and contrib are enabled.
 
 dpkg --add-architecture i386
@@ -11,5 +22,6 @@ apt dist-upgrade -y
 apt install -y aptitude git curl wget vim sudo fakeroot p7zip-full zip unzip strace lsof htop nmap build-essential tshark intel-microcode open-cobol lua5.3 gfortran-7
 # High-level
 apt install -y wireshark qbittorrent gimp vlc libdvdnav4 libdvdread4 libdvdcss2 libbluray2
+dpkg-reconfigure libdvd-pkg
 
 exit 0
