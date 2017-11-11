74ae9332090c363dc8d8260cbe9f8ad69f73cb84
Matthew <Bean6754@users.noreply.github.com>
2017-11-11 00:02:08 +0000

    Update Debian-Buster-KDE.sh

--- Debian-Buster-KDE.sh
+++ Debian-Buster-KDE.sh
@@ -17,7 +17,7 @@ apt upgrade -y
 apt dist-upgrade -y
 
 # Low-level
-apt install -y aptitude git curl wget vim sudo fakeroot p7zip-full zip unzip strace lsof htop nmap build-essential tshark intel-microcode open-cobol lua5.3 gfortran-7
+apt install -y aptitude git curl wget vim sudo fakeroot p7zip-full zip unzip strace lsof htop nmap build-essential tshark intel-microcode open-cobol lua5.3 gfortran-7 gdisk
 # High-level
 apt install -y wireshark qbittorrent gimp vlc libdvdnav4 libdvdread4 libdvdcss2 libbluray2 redshift plasma-applet-redshift-control
 dpkg-reconfigure libdvd-pkg
