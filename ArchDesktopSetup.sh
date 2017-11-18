f49a17d3abef7edd59b1f522874b00319981d03a
Matthew <Bean6754@users.noreply.github.com>
2017-11-18 00:31:46 +0000

    Update ArchDesktopSetup.sh

--- ArchDesktopSetup.sh
+++ ArchDesktopSetup.sh
@@ -9,51 +9,23 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
-# ...
 
-# Set timezone
-rm -R /etc/timezone
-ln -s /usr/share/Europe/London /etc/timezone
-
-# Set hwclock to UTC.
-hwclock --utc
-
-# Date timestamp.
-datestamp() {
-  date +"%D"
-}
-echo 'The date is' & datestamp
-
-# Enable root access.
-su
-
-systemctl enable dhcpcd.service
-systemctl start dhcpcd.service
-# Check internet connectivity
-
-
-cd /etc/
-cp -r pacman.conf pacman.conf.old
-curl (insert pacman.conf url here) -o pacman.conf
 pacman -Syyu
-pacman -S xorg xorg-xinit i3 yaourt aurvote customizepkg rsync
+pacman -S xorg xorg-xinit i3 i3lock i3status yaourt aurvote customizepkg rsync
 
 # Disable root access for the AUR.
 exit
 
 yaourt -S cower-git pacaur-git
 pacaur -Rsnc yaourt
-pacaur -S vlc libdvdcss links ranger w3m ttf-liberation ttf-dejavu ttf-freefont i3blocks lemonbar-git feh git curl wget htop strace lsof
+pacaur -S vim python2 python ruby perl lua tcl vlc pulseaudio pulseaudio-alsa pavucontrol libdvdnav libdvdread libbluray libdvdcss p7zip rxvt-unicode links ranger w3m ttf-liberation ttf-dejavu ttf-freefont i3blocks lemonbar-git feh git curl wget htop strace lsof
+pacaur -S gimp gimp-help-en_gb libreoffice-fresh libreoffice-fresh-en-GB hunspell hunspell-en_GB firefox-developer-en-gb ffmpeg flashplayer-standalone
+# libva-vdpau-driver for nvidia and libva-intel-driver for Intel.
+# For i3blocks.
+pacaur -S acpi bc lm_sensors openvpn playerctl alsa-utils sysstat
 
 # Change to user home folder.
-cd ~/
+cd ~
 touch .xinitrc
 echo 'exec i3' > .xinitrc
 echo 'startx' >> .bashrc
-
-
-
-# i3 stuff.
-cd ~/config/.i3/
-curl (insert i3 'config' url here) -o config
-curl (inser i3blocks.conf url here) -o i3blocks.conf
