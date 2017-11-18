b9a525292cabd8df805e16a597f590fe2361f280
Matthew <Bean6754@users.noreply.github.com>
2017-11-18 01:14:26 +0000

    Update ArchDesktopSetup.sh

--- ArchDesktopSetup.sh
+++ ArchDesktopSetup.sh
@@ -18,7 +18,7 @@ exit
 
 yaourt -S cower-git pacaur-git
 pacaur -Rsnc yaourt
-pacaur -S vim python2 python ruby perl lua tcl vlc-qt5 qt5-base pulseaudio pulseaudio-alsa pavucontrol libdvdnav libdvdread libbluray libdvdcss p7zip zip unzip rxvt-unicode links ranger w3m ttf-liberation ttf-dejavu ttf-freefont dmenu rofi redshift i3blocks lemonbar-git feh git curl wget htop strace lsof
+pacaur -S vim python2 python ruby perl lua tcl vlc qt4 qt5-base pulseaudio pulseaudio-alsa pavucontrol libdvdnav libdvdread libbluray libdvdcss p7zip zip unzip rxvt-unicode links ranger w3m ttf-liberation ttf-dejavu ttf-freefont dmenu rofi redshift i3blocks lemonbar-git feh git curl wget htop strace lsof
 pacaur -S gimp gimp-help-en_gb libreoffice-fresh libreoffice-fresh-en-GB hunspell hunspell-en_GB firefox-developer-en-gb ffmpeg flashplayer-standalone jdk9-openjdk transmission-cli transmission-gtk skypeforlinux-bin weechat
 # libva-vdpau-driver for nvidia and libva-intel-driver for Intel.
 # For i3blocks.
