80c758bd29832f053a63cc8e4d6297fed413c239
Matthew <Bean6754@users.noreply.github.com>
2017-11-23 21:14:53 +0000

    Create Fedora-27-Server.sh

---
+++ Fedora-27-Server.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# Repos.
+# RPMFusion.
+dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+# Update repositories and any potential packages.
+dnf update -y
+# Group packages.
+dnf groupinstall -y "Development-Tools" "Security-Lab"
+# Low level.
+dnf install -y kernel-devel strace lsof htop git curl wget vim emacs-nox transmission-cli gcc-c++ ruby nmap p7zip p7zip-plugins zip unzip wireshark wireshark-cli
+
+exit 0
