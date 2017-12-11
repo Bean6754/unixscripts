917aec36eb6a8f27b76e0c2687509c85e7a1ad32
Matthew <Bean6754@users.noreply.github.com>
2017-12-11 11:46:06 +0000

    Update openSUSE-Tumbleweed-Desktop.sh

--- openSUSE-Tumbleweed-Desktop.sh
+++ openSUSE-Tumbleweed-Desktop.sh
@@ -11,6 +11,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Install development tools.
+zypper install -t pattern devel_basis
 zypper install -y make cmake git curl wget vim python2 python3 ruby gcc gcc-c++ lua tcl
 
 # Networking tools.
@@ -20,11 +21,11 @@ zypper install -y make cmake git curl wget vim python2 python3 ruby gcc gcc-c++ 
 echo Type 'a' then press the enter key
 zypper ar -f http://opensuse-guide.org/repo/openSUSE_Tumbleweed/ libdvdcss
 zypper ref
-zypper install -y libdvdcss2 ffmpeg lame gstreamer-plugins-libav gstreamer-plugins-bad gstreamer-plugins-ugly gstreamer-plugins-ugly-orig-addon vlc vlc-codecs libxine2 libxine2-codecs
+zypper install -y libdvdnav4 libdvdread4 libdvdcss2 ffmpeg lame gstreamer gstreamer-plugins-libav gstreamer-plugins-good gstreamer-plugins-bad gstreamer-plugins-ugly vlc vlc-codec-gstreamer
 
 # Adobe Flash.
 zypper ar --check --refresh http://linuxdownload.adobe.com/linux/x86_64/ adobe
-rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
+# rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
 zypper ref
 # rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
 zypper install adobe-release-x86_64 flash-plugin
