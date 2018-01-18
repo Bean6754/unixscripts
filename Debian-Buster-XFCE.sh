33533327608f70e4fa10efb6fe053f883301354d
Matthew <Bean6754@users.noreply.github.com>
2018-01-18 23:54:44 +0000

    Update Debian-Buster-XFCE.sh

--- Debian-Buster-XFCE.sh
+++ Debian-Buster-XFCE.sh
@@ -17,7 +17,7 @@ apt upgrade -y
 apt dist-upgrade -y
 
 # Low-level
-apt install -y aptitude git curl wget vim emacs sudo fakeroot p7zip-full zip unzip strace lsof htop nmap build-essential tshark intel-microcode open-cobol lua5.3 gfortran-7 gdisk
+apt install -y aptitude git curl wget vim emacs sudo fakeroot p7zip-full zip unzip strace lsof htop nmap build-essential ruby tshark intel-microcode open-cobol lua5.3 gfortran-7 gdisk
 # High-level
 apt install -y wireshark-gtk transmission transmission-cli gimp vlc libdvdnav4 libdvdread4 libdvdcss2 libbluray2 redshift redshift-gtk geany pidgin firefox-esr firefox-esr-l10n-en-gb
 dpkg-reconfigure libdvd-pkg
