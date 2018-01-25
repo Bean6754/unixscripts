c5235dece5b9b6b1a63a87bf01a165698fff8056
Matthew <Bean6754@users.noreply.github.com>
2018-01-25 23:49:41 +0000

    Update Debian-Buster-LXQt.sh

--- Debian-Buster-LXQt.sh
+++ Debian-Buster-LXQt.sh
@@ -19,8 +19,8 @@ apt dist-upgrade -y
 # Low-level
 apt install -y aptitude git curl wget vim emacs-nox sudo fakeroot p7zip-full zip unzip strace lsof htop nmap build-essential ruby tshark intel-microcode lua5.3 gdisk tftp ftp tcpdump
 # High-level
-# Find Qt alternative for gparted, geany and filezilla
-apt install -y wireshark-qt transmission-qt transmission-cli krita mpv libdvdnav4 libdvdread4 libdvdcss2 libbluray2 redshift quassel firefox-esr firefox-esr-l10n-en-gb
+# Find Qt alternative for gparted and filezilla
+apt install -y wireshark-qt transmission-qt transmission-cli krita mpv libdvdnav4 libdvdread4 libdvdcss2 libbluray2 redshift quassel qtcreator firefox-esr firefox-esr-l10n-en-gb
 dpkg-reconfigure libdvd-pkg
 # Flash player.
 apt install -y browser-plugin-freshplayer-pepperflash
