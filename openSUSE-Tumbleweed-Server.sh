a36fa2417df29f4a8104d392216f9e40776682e5
Matthew <Bean6754@users.noreply.github.com>
2018-01-27 12:40:32 +0000

    Create openSUSE-Tumbleweed-Server.sh

---
+++ openSUSE-Tumbleweed-Server.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# Update system.
+zypper up -y
+
+# Install development tools.
+zypper install -t pattern devel_basis
+zypper install -y make cmake git curl wget vim emacs-nox python2 python3 ruby gcc gcc-c++ lua tcl
+
+# Networking tools.
+zypper in -y wireshark tcpdump
+
+# Install neofetch and run it.
+git clone https://github.com/dylanaraps/neofetch.git
+cd neofetch
+make install
+cd ..
+rm -rf neofetch
+clear
+neofetch
