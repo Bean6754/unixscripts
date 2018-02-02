628f71a730cd401a48dfa894bb2edb7de360b48f
Matthew <Bean6754@users.noreply.github.com>
2018-02-02 20:58:25 +0000

    Update and rename Fedora-27-XFCE.sh to Fedora-27-Desktop.sh

--- Fedora-27-Desktop.sh
+++ Fedora-27-Desktop.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Make sure only root can run our script
 if [ "$(id -u)" != "0" ]; then
@@ -11,8 +11,10 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Repos.
-# negativo17 Steam repo.
-dnf config-manager --add-repo=https://negativo17.org/repos/fedora-steam.repo
+# RPMFusion
+# dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+# Fedy
+ dnf -y install https://dl.folkswithhats.org/fedora/$(rpm -E %fedora)/RPMS/folkswithhats-release.noarch.rpm 
 # Update repositories and any potential packages.
 dnf update -y
 # Group packages.
@@ -20,10 +22,8 @@ dnf groupinstall -y "Development-Tools" "Security-Lab"
 # Low level.
 dnf install -y kernel-devel kernel-headers acpid dkms strace lsof htop git curl wget vim transmission-cli gcc-c++ ruby nmap libdvdnav libdvdread libbluray p7zip p7zip-plugins zip unzip wireshark wireshark-cli
 # High level.
-dnf install -y gimp transmission pavucontrol wireshark-gtk steam
+dnf install -y gnome-tweak-tool gimp transmission pavucontrol wireshark-gtk steam
 # Adobe Flash.
 rpm -ivh http://linuxdownload.adobe.com/adobe-release/adobe-release-x86_64-1.0-1.noarch.rpm
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
 dnf install flash-plugin alsa-plugins-pulseaudio libcurl
-
-exit 0
