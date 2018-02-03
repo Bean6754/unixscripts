dabdfe6a64693a56e4593f01277b974b774e0d6b
Matthew <Bean6754@users.noreply.github.com>
2018-02-03 23:31:25 +0000

    Update Fedora-27-Desktop.sh

--- Fedora-27-Desktop.sh
+++ Fedora-27-Desktop.sh
@@ -32,5 +32,9 @@ dnf install -y flash-plugin alsa-plugins-pulseaudio libcurl
 dnf install -y HandBrake-gui HandBrake-cli makemkv libdvdcss libbluray ffmpeg gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-bad gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-ugly-free GraphicsMagick
 # PlayOnLinux.
 dnf install -y playonlinux
+ # Discord. (Run as user)
+ # git clone https://github.com/RPM-Outpost/discord.git
+ # cd discord
+ # ./create-package.sh canary
 # Autoremove any unneeded dependancies.
 dnf autoremove -y
