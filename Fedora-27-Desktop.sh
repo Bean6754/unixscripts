6aa8330198b58ff785f59a597aa18efdc81f11cf
Matthew <Bean6754@users.noreply.github.com>
2018-02-03 15:52:09 +0000

    Update Fedora-27-Desktop.sh

--- Fedora-27-Desktop.sh
+++ Fedora-27-Desktop.sh
@@ -30,8 +30,7 @@ rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
 dnf install -y flash-plugin alsa-plugins-pulseaudio libcurl
 # Multimedia.
 dnf install -y HandBrake-gui HandBrake-cli makemkv libdvdcss libbluray ffmpeg gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-bad gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-ugly-free GraphicsMagick
-# Wine.
-dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/26/winehq.repo
+# PlayOnLinux.
 dnf install -y playonlinux
 # Autoremove any unneeded dependancies.
 dnf autoremove -y
