519a677afeddace2e6218115630f6826b20b6042
Matthew <Bean6754@users.noreply.github.com>
2018-02-11 01:02:25 +0000

    Update Debian-Buster-KDE.sh

--- setup-scripts/Debian-Buster-KDE.sh
+++ setup-scripts/Debian-Buster-KDE.sh
@@ -20,7 +20,7 @@ apt dist-upgrade -y
 apt install -y aptitude git curl wget vim emacs-nox sudo fakeroot p7zip-full zip unzip strace lsof htop nmap build-essential ruby tshark intel-microcode lua5.3 gdisk tftp ftp tcpdump qbittorrent-nox
 # High-level
 # Find Qt alternative for gparted and filezilla
-apt install -y wireshark-qt qbittorrent qupzilla krita kdenlive simplescreenrecorder vlc libdvdnav4 libdvdread4 libdvdcss2 libbluray2 redshift quassel qtcreator firefox-esr firefox-esr-l10n-en-gb
+apt install -y wireshark-qt qbittorrent qupzilla krita kdenlive simplescreenrecorder vlc libdvdnav4 libdvdread4 libdvdcss2 libbluray2 redshift plasma-applet-redshift-control quassel qtcreator firefox-esr firefox-esr-l10n-en-gb
 dpkg-reconfigure libdvd-pkg
 # Flash player.
 apt install -y browser-plugin-freshplayer-pepperflash
