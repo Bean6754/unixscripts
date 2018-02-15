99332a1aef650d59437c49109c5411548adb753f
Matthew <Bean6754@users.noreply.github.com>
2018-02-15 22:49:27 +0000

    Update openSUSE-Tumbleweed-nvidia.sh

--- setup-scripts/openSUSE-Tumbleweed-nvidia.sh
+++ setup-scripts/openSUSE-Tumbleweed-nvidia.sh
@@ -2,10 +2,14 @@
 
 zypper addrepo --refresh http://http.download.nvidia.com/opensuse/tumbleweed NVIDIA
 
-
 clear
 echo On the next command, type 'a' and press enter.
 echo Then on the licensing pages press 'q', then type yes. Twice!
 sleep 4
 
 zypper install-new-recommends
+
+
+# For KDE (Plasma 5)
+systemctl disable sddm
+systemctl set-default multi-user.target
