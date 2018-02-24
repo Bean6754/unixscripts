2e787bc45bf04d2c9db322c94b652cf58a5067af
Matthew <Bean6754@users.noreply.github.com>
2018-02-24 11:32:48 +0000

    Update GentooDesktopSetup.sh

--- setup-scripts/GentooDesktopSetup.sh
+++ setup-scripts/GentooDesktopSetup.sh
@@ -1,5 +1,3 @@
-## DO NOT USE YET!
-
 #!/bin/bash
 
 # Make sure only root can run our script
@@ -12,37 +10,15 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# make.conf stuff.
-echo 'CFLAGS="-march=native -O2 -pipe"' > /etc/portage/make.conf
-echo 'CXXFLAGS="${CFLAGS}"' >> /etc/portage/make.conf
-echo 'CHOST="x86_64-pc-linux-gnu"' >> /etc/portage/make.conf
-echo 'USE="bindlist dbus -systemd -selinux -qt4 -qt5 -kde -gnome alsa cdr dvd offensive pulseaudio"' >> /etc/portage/make.conf
-echo 'CPU_FLAGS_X86="mmx sse sse2"' >> /etc/portage/make.conf
-echo 'PORTDIR="/usr/portage"' >> /etc/portage/make.conf
-echo 'DISTDIR="${PORTDIR}/distfiles"' >> /etc/portage/make.conf
-echo 'PKGDIR="${PORTDIR}/packages"' >> /etc/portage/make.conf
-emerge -v app-portage/cpuid2cpuflags
-cpuinfo2cpuflags-x86 >> /etc/portage/make.conf
-#cat /proc/cpuinfo | grep processor | wc -l >> /etc/portage/make.conf
-echo 'MAKEOPTS="-j4"' >> /etc/portage/make.conf
-echo 'GENTOO_MIRRORS="ftp://mirror.bytemark.co.uk/gentoo/ http://mirror.bytemark.co.uk/gentoo/ rsync://mirror.bytemark.co.uk/gentoo/"' >> /etc/portage/make.conf
-echo 'VIDEO_CARDS="nvidia"' >> /etc/portage/make.conf
-echo 'ACCEPT_LICENSE="*"' >> /etc/portage/make.conf
-echo 'GRUB_PLATFORMS="efi-64"' >> /etc/portage/make.conf
-echo 'ABI_X86="amd64"' >> /etc/portage/make.conf
-
-eselect profile set 1
+# Update system.
 emerge --update --deep --newuse @world
 
-rm -rf /etc/timezone
-echo "Europe/London" > /etc/timezone
-emerge --config sys-libs/timezone-data
-echo 'en_GB.UTF-8 UTF-8' > /etc/locale.gen
-locale-gen
-eselect locale set 3
-env-update && source /etc/profile
-
-emerge -v app-admin/sudo dev-util/cmake dev-vcs/git sys-devel/autoconf sys-devel/autogen sys-devel/automake sys-devel/binutils sys-devel/m4 sys-devel/make sys-devel/patch sys-devel/gcc x11-base/xorg-x11 media-fonts/droid media-fonts/noto www-client/firefox unrar rar zip unzip p7zip xarchiver rxvt-unicode rofi feh redshift i3 i3lock i3status i3blocks media-libs/alsa-lib media-sound/alsa-utils media-plugins/alsa-plugins media-sound/pulseaudio pavucontrol htop dbus strace x11-drivers/nvidia-drivers
+# Portage utils.
+emerge -v app-portage/portage-utils app-portage/cpuid2cpuflags
+# Low-level.
+emerge -v app-admin/sudo dev-util/cmake dev-vcs/git sys-devel/autoconf sys-devel/autogen sys-devel/automake sys-devel/binutils sys-devel/m4 sys-devel/make sys-devel/patch sys-devel/gcc htop dbus strace
+# High-level.
+emerge -v x11-base/xorg-x11 media-fonts/droid media-fonts/noto www-client/firefox unrar rar zip unzip p7zip xarchiver rxvt-unicode rofi feh redshift i3 i3lock i3status i3blocks media-libs/alsa-lib media-sound/alsa-utils media-plugins/alsa-plugins media-sound/pulseaudio pavucontrol x11-drivers/nvidia-drivers
 
 eselect opengl set nvidia
 eselect opencl set nvidia
@@ -51,5 +27,3 @@ rc-update add dbus default
 rc-update add consolekit default
 
 #gpasswd -d $USER audio
-
-exit 0
