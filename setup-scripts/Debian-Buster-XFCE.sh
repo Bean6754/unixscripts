75ab780158610c968ea7e7df2bdbd6d7a48af6eb
Matthew <Bean6754@users.noreply.github.com>
2018-03-02 20:34:24 +0000

    Update Debian-Buster-XFCE.sh

--- setup-scripts/Debian-Buster-XFCE.sh
+++ setup-scripts/Debian-Buster-XFCE.sh
@@ -19,7 +19,7 @@ apt full-upgrade -y
 apt autoremove -y
 
 # Low-level
-apt install -y aptitude git curl wget vim emacs-nox sudo fakeroot p7zip-full zip unzip strace lsof htop screen tmux nmap build-essential ruby tshark intel-microcode lua5.3 gdisk tftp ftp tcpdump transmission-cli nethogs iftop software-properties-common
+apt install -y aptitude git curl wget vim emacs-nox sudo fakeroot p7zip-full zip unzip strace lsof htop screen tmux nmap build-essential ruby tshark intel-microcode lua5.3 gdisk tftp ftp tcpdump transmission-cli net-tools nethogs iftop software-properties-common
 # High-level
 apt install -y wireshark-gtk transmission-gtk pavucontrol xarchiver geany geany-plugins baobab filezilla gparted gimp redshift redshift-gtk pidgin firefox-esr firefox-esr-l10n-en-gb thunderbird thunderbird-l10n-en-gb gpick software-properties-gtk synaptic gdebi conky-all guvcview simplescreenrecorder wmctrl
 # Codecs
