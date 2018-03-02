1464b2d383026b4289ea9cfbcab34ac7085730d2
Matthew <Bean6754@users.noreply.github.com>
2018-03-02 01:34:12 +0000

    Update Debian-Buster-XFCE.sh

--- setup-scripts/Debian-Buster-XFCE.sh
+++ setup-scripts/Debian-Buster-XFCE.sh
@@ -21,7 +21,7 @@ apt autoremove -y
 # Low-level
 apt install -y aptitude git curl wget vim emacs-nox sudo fakeroot p7zip-full zip unzip strace lsof htop nmap build-essential ruby tshark intel-microcode lua5.3 gdisk tftp ftp tcpdump transmission-cli nethogs iftop software-properties-common
 # High-level
-apt install -y wireshark-gtk transmission-gtk pavucontrol xarchiver geany geany-plugins filezilla gparted gimp redshift redshift-gtk pidgin firefox-esr firefox-esr-l10n-en-gb thunderbird thunderbird-l10n-en-gb gpick software-properties-gtk synaptic gdebi conky-all guvcview simplescreenrecorder
+apt install -y wireshark-gtk transmission-gtk pavucontrol xarchiver geany geany-plugins filezilla gparted gimp redshift redshift-gtk pidgin firefox-esr firefox-esr-l10n-en-gb thunderbird thunderbird-l10n-en-gb gpick software-properties-gtk synaptic gdebi conky-all guvcview simplescreenrecorder wmctrl
 # Codecs
 apt install -y ffmpeg libdvdnav4 libdvdread4 libdvdcss2 libbluray2
 dpkg-reconfigure libdvd-pkg
