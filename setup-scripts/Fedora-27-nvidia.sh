b241f4395ed2e296346ce996dc13a2b988a9434f
Matthew <Bean6754@users.noreply.github.com>
2018-03-07 23:02:25 +0000

    Update Fedora-27-nvidia.sh

--- setup-scripts/Fedora-27-nvidia.sh
+++ setup-scripts/Fedora-27-nvidia.sh
@@ -15,6 +15,6 @@ dnf update -y
 # Install repo.
 dnf config-manager --add-repo=https://negativo17.org/repos/fedora-nvidia.repo
 # Install drivers and CUDA.
-dnf install -y nvidia-driver nvidia-settings kernel-devel akmod-nvidia vulkan vulkan.i686 nvidia-driver-libs nvidia-driver-libs.i686 cuda nvidia-driver-cuda cuda-devel cuda-cudart
+dnf install -y nvidia-driver nvidia-xconfig nvidia-settings kernel-devel akmod-nvidia vulkan vulkan.i686 nvidia-driver-libs nvidia-driver-libs.i686 cuda nvidia-driver-cuda cuda-devel cuda-cudart
 # Autoremove any unneeded dependancies.
 dnf autoremove -y
