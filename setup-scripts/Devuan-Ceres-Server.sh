9b9d8fc3e5cd101df4f4c0e2f227eafcd492fd05
Matthew <Bean6754@users.noreply.github.com>
2018-03-10 13:37:56 +0000

    Update Devuan-Ceres-Server.sh

--- setup-scripts/Devuan-Ceres-Server.sh
+++ setup-scripts/Devuan-Ceres-Server.sh
@@ -16,6 +16,8 @@ apt autoremove -y
 
 # Low-level
 apt install -y aptitude git curl wget vim emacs-nox sudo fakeroot p7zip-full zip unzip strace lsof htop screen tmux nmap build-essential ruby tshark intel-microcode lua5.3 gdisk tftp ftp tcpdump transmission-cli net-tools nethogs iftop software-properties-common ntp exif imagemagick
+# Security.
+apt install -y chkrootkit clamav snort
 # Java.
 apt purge -y openjdk*
 apt autoremove -y
