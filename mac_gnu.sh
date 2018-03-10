df0df3a3196bf079069900bb4568ce83853b45a7
Matthew <Bean6754@users.noreply.github.com>
2018-03-10 18:59:02 +0000

    Update mac_gnu.sh

--- mac_gnu.sh
+++ mac_gnu.sh
@@ -55,5 +55,4 @@ brew install zsh
 # git curl wget python, already selected
 brew install htop lua python2 python3 nmap nethogs iftop arping arpoison arp-scan tmux
 # Security.
-brew install chkrootkit clamav snort
-sudo chmod o+r /dev/bpf*
+brew install clamav suricata
