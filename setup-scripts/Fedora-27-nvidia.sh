768a6aeec35cc9c865677a3bf16ab1ed4ebfbfe8
Matthew <Bean6754@users.noreply.github.com>
2018-03-11 21:41:48 +0000

    Update Fedora-27-nvidia.sh

--- setup-scripts/Fedora-27-nvidia.sh
+++ setup-scripts/Fedora-27-nvidia.sh
@@ -18,3 +18,8 @@ dnf config-manager --add-repo=https://negativo17.org/repos/fedora-nvidia.repo
 dnf install -y nvidia-driver nvidia-xconfig nvidia-settings kernel-devel akmod-nvidia vulkan vulkan.i686 nvidia-driver-libs nvidia-driver-libs.i686 cuda nvidia-driver-cuda cuda-devel cuda-cudart
 # Autoremove any unneeded dependancies.
 dnf autoremove -y
+
+echo 'omit_drivers+="nouveau"' > /etc/dracut.conf.d/blacklist-nouveau.conf
+mkinitrd --force /boot/initramfs-$(uname -r).img $(uname -r)
+reboot
+/usr/sbin/akmods --force
