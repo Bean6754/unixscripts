298960d3d3453966e5cac885504f7d3811d1bbb0
Matthew <Bean6754@users.noreply.github.com>
2018-03-12 19:57:34 +0000

    Update and rename Fedora-27-KDE-Desktop.sh to Fedora-27-XFCE-Desktop.sh

--- setup-scripts/Fedora-27-XFCE-Desktop.sh
+++ setup-scripts/Fedora-27-XFCE-Desktop.sh
@@ -21,8 +21,9 @@ dnf install -y kernel-devel kernel-headers acpid dkms strace lsof htop git curl 
 # Network monitoring tools.
 dnf install -y nethogs iftop
 # High level.
-dnf install -y setroubleshoot qt5ct qt5-qtconfiguration libreoffice libreoffice-langpack-en gimp inkscape inkscape-psd krita transmission-qt pavucontrol-qt wireshark-qt steam vulkan vulkan.i686 qt-creator kde-partitionmanager quassel kdenlive simplescreenrecorder filezilla redshift plasma-applet-redshift-control
+# KDE: dnf install -y setroubleshoot qt5ct qt5-qtconfiguration libreoffice libreoffice-langpack-en gimp inkscape inkscape-psd krita transmission-qt pavucontrol-qt wireshark-qt steam vulkan vulkan.i686 qt-creator kde-partitionmanager quassel kdenlive simplescreenrecorder filezilla redshift plasma-applet-redshift-control
 # GNOME: dnf install -y gnome-tweak-tool gimp transmission pavucontrol wireshark-gtk steam vulkan vulkan.i686 gnome-builder geany glade gparted guvcview polari pitivi qt5ct kdenlive simplescreenrecorder filezilla redshift redshift-gtk conky conky-manager
+dnf install -y setroubleshoot libreoffice libreoffice-langpack-en gimp transmission-gtk pavucontrol wireshark-gtk steam vulkan vulkan.i686 geany glade gparted guvcview kdenlive simplescreenrecorder filezilla redshift redshift-gtk conky conky-manager
 setsebool -P selinuxuser_execheap 1
 if [[ $(grep QT_QPA_PLATFORMTHEME /etc/environment) = *QT_QPA_PLATFORMTHEME* ]]; then
    echo
@@ -32,8 +33,8 @@ fi
 # Fonts.
 dnf install -y liberation-fonts-common dejavu-fonts-common google-noto-fonts-common google-noto-emoji-fonts google-noto-mono-fonts google-noto-cjk-fonts-common
 # Themes.
-dnf install -y plasma-breeze sddm-breeze sddm-themes numix-icon-theme numix-icon-theme-circle moka-icon-theme paper-icon-theme tango-icon-theme tango-icon-theme-extras faience-icon-theme
-# GNOME: dnf install -y adwaita-cursor-theme adwaita-gtk2-theme adwaita-icon-theme gtk-murrine-engine numix-gtk-theme numix-icon-theme numix-icon-theme-circle moka-icon-theme paper-icon-theme tango-icon-theme tango-icon-theme-extras faience-icon-theme
+# KDE: dnf install -y plasma-breeze sddm-breeze sddm-themes numix-icon-theme numix-icon-theme-circle moka-icon-theme paper-icon-theme tango-icon-theme tango-icon-theme-extras faience-icon-theme
+dnf install -y adwaita-cursor-theme adwaita-gtk2-theme adwaita-icon-theme gtk-murrine-engine numix-gtk-theme numix-icon-theme numix-icon-theme-circle moka-icon-theme paper-icon-theme tango-icon-theme tango-icon-theme-extras faience-icon-theme
 # Adobe Flash.
 rpm -ivh http://linuxdownload.adobe.com/adobe-release/adobe-release-x86_64-1.0-1.noarch.rpm
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
