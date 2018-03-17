7c9b1c8df97bcbca71d4edf2f689676a867f373b
Matthew <Bean6754@users.noreply.github.com>
2018-03-17 20:17:36 +0000

    Update Debian-Buster-Server.sh

--- setup-scripts/Debian-Buster-Server.sh
+++ setup-scripts/Debian-Buster-Server.sh
@@ -24,7 +24,8 @@ apt purge -y openjdk*
 apt autoremove -y
 apt install -y openjdk-8-jdk
 # Server specific stuff.
-apt install -y mariadb-server mariadb-client php7.1 php7.1-mysql
-systemctl enable apache2
+apt install -y mariadb-server mariadb-client php7.1 php7.1-mysql nginx
+systemctl disable apache2
+systemctl enable nginx
 systemctl enable mariadb
 mysql_secure_installation
