9948f1d9b3e5492b5ff861621a728cde656bbae7
Matthew <Bean6754@users.noreply.github.com>
2018-03-23 13:35:42 +0000

    Update pkill.sh
    Removed root code, root isn't needed but would help.

--- Security/pkill.sh
+++ Security/pkill.sh
@@ -1,17 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 # WARNING: This script will kill most processes on the system.
 # It is best run as root, but it also works in userland.
-
-: '
-Make sure only root can run our script
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi '
 
 var=2
 while true
