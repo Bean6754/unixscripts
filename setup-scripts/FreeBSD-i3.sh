39c3e77e933ee9bd0036b15d3a75e4cc7b50bbe7
Matthew <Bean6754@users.noreply.github.com>
2018-03-24 18:27:02 +0000

    Create FreeBSD-i3.sh

---
+++ setup-scripts/FreeBSD-i3.sh
@@ -0,0 +1,38 @@
+#!/bin/tcsh
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+portsnap fetch
+portsnap extract
+portsnap update
+
+pkg update
+pkg upgrade -y
+
+# https://www.textfixer.com/tools/alphabetize-text-words.php
+# Low-level.
+pkg install -y curl git ImageMagick neofetch p7zip rar sudo tmux unrar unzip vim-lite wget zip
+# High-level.
+pkg install -y feh firefox i3 i3lock i3status libbluray libdvdcss libdvdnav libdvdread mpv playonbsd rofi rxvt-unicode xarchiver xinit xorg
+
+echo 'proc /proc procfs rw,noauto 0 0' >> /etc/fstab
+
+echo 'linux_enable="YES"' >> /etc/rc.conf
+echo 'dbus_enable="YES"' >> /etc/rc.conf
+echo 'hald_enable="YES"' >> /etc/rc.conf
+
+# NVIDIA-Driver Stuff.
+# kldload linux
+# kldload linux64
+# pkg install -y nvidia-driver nvidia-settings nvidia-texture-tools nvidia-xconfig
+# echo 'nvidia_load="YES"' >> /boot/loader.conf
+# echo 'nvidia_name="nvidia"' >> /boot/loader.conf
+# echo 'nvidia_modeset_load="YES"' >> /boot/loader.conf
+# echo 'nvidia_modeset_name="nvidia-modeset"' >> /boot/loader.conf
+# echo 'kld_list="nvidia nvidia-modeset"' >> /etc/rc.conf
+# echo 'nvidia_name="nvidia"' >> /etc/rc.conf
+# echo 'nvidia_modeset_name="nvidia-modeset"' >> /etc/rc.conf
