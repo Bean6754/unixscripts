4cd8668e30c174088ee5a356de367e84c2b4f25b
Matthew <Bean6754@users.noreply.github.com>
2018-03-27 17:24:32 +0100

    Update mac.sh

--- setup-scripts/mac.sh
+++ setup-scripts/mac.sh
@@ -8,15 +8,24 @@ sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
 # Install MacPorts.
 curl -L -O https://github.com/macports/macports-base/releases/download/v2.4.2/MacPorts-2.4.2.tar.bz2
 tar xjvf MacPorts-2.4.2.tar.bz2
+cd MacPorts-2.4.2
+./configure
+make
+sudo make install
+cd ..
+rm -rf MacPorts-2.4.2*
 
 # Fix Vim backspace issue.
 echo "set backspace=2" >> ~/.vimrc
+
+# Just in case..
+sudo rm -rf /opt/local/var/macports/registry/.registry.lock
 
 # Update MacPorts.
 sudo /opt/local/bin/port -v selfupdate
 sudo /opt/local/bin/port -v upgrade outdated
 # Install software.
-sudo /opt/local/bin/port -v install arping arp-scan clamav dsniff ettercap iftop iterm2 htop lua neofetch nmap python27 python36 tcsh tmux vim
+sudo /opt/local/bin/port -v install arping arp-scan clamav dsniff ettercap iftop iTerm2 htop lua neofetch nmap python27 python36 tcsh tmux vim
 # Setup python2 and python3.
 sudo /opt/local/bin/port select --set python python27
 sudo /opt/local/bin/port select --set python3 python36
