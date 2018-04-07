143403f3c1110510e77547db0307e59d0a42f757
Matthew <Bean6754@users.noreply.github.com>
2018-04-07 14:05:04 +0100

    Update ArchDesktopSetup.sh

--- setup-scripts/ArchDesktopSetup.sh
+++ setup-scripts/ArchDesktopSetup.sh
@@ -13,7 +13,7 @@ su -c 'pacman -S xorg xorg-xinit i3 i3lock i3status yaourt aurvote customizepkg 
 
 yaourt -S cower-git pacaur-git
 pacaur -Rsnc yaourt
-pacaur -S vim scrot python2 python ruby perl lua tcl vlc-git qt5-base pulseaudio pulseaudio-alsa pavucontrol libdvdnav libdvdread libbluray libdvdcss p7zip zip unzip rxvt-unicode links ranger w3m ttf-liberation ttf-dejavu ttf-freefont dmenu rofi redshift i3blocks lemonbar-git feh git curl wget htop strace lsof
+pacaur -S openssh vim scrot python-pip python2-pip python python2 ruby perl lua tcl mpv qt5-base pulseaudio pulseaudio-alsa pavucontrol libdvdnav libdvdread libbluray libdvdcss p7zip zip unzip rxvt-unicode links ranger w3m ttf-liberation ttf-dejavu ttf-freefont dmenu rofi redshift i3blocks lemonbar-git feh git curl wget htop strace lsof
 pacaur -S gimp gimp-help-en_gb libreoffice-fresh libreoffice-fresh-en-GB hunspell hunspell-en_GB firefox-developer-en-gb ffmpeg flashplayer-standalone jdk9-openjdk transmission-cli transmission-gtk skypeforlinux-bin weechat
 # libva-vdpau-driver for nvidia and libva-intel-driver for Intel.
 # For i3blocks.
@@ -22,3 +22,6 @@ pacaur -S acpi bc lm_sensors openvpn playerctl alsa-utils sysstat
 touch ~/.xinitrc
 echo 'exec i3' > ~/.xinitrc
 echo 'startx' >> ~/.bashrc
+
+# NVIDIA.
+# su -c 'pacman -S nvidia nvidia-settings opencl-nvidia'
