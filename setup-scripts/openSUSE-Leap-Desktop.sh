413ce671f57dec7599cdce829c811945831675ec
Matthew <Bean6754@users.noreply.github.com>
2018-04-10 23:58:14 +0100

    Update openSUSE-Leap-Desktop.sh

--- setup-scripts/openSUSE-Leap-Desktop.sh
+++ setup-scripts/openSUSE-Leap-Desktop.sh
@@ -17,24 +17,22 @@ zypper dup
 
 # Low-level and development tools.
 zypper install -t pattern devel_basis
-zypper install -y make cmake git curl wget lsof strace htop vim emacs-nox python2 python3 ruby gcc gcc-c++ lua tcl java-1_8_0-openjdk java-1_8_0-openjdk-devel qbittorrent-nox
+zypper install -y make cmake git curl wget lsof strace htop vim emacs-nox python2 python3 ruby gcc gcc-c++ lua tcl java-11-openjdk java-11-openjdk-devel qbittorrent-nox
 
 # Networking tools.
 zypper in -y wireshark tcpdump
 
 # High-level and multimedia codecs.
-echo Type 'a' then press the enter key
-zypper ar -f http://opensuse-guide.org/repo/openSUSE_Tumbleweed/ libdvdcss
-zypper ref
-zypper install -y libdvdnav4 libdvdread4 libdvdcss2 ffmpeg lame gstreamer gstreamer-plugins-libav gstreamer-plugins-good gstreamer-plugins-bad gstreamer-plugins-ugly redshift qbittorrent vlc vlc-codec-gstreamer simplescreenrecorder kdenlive firewall-config
+wget http://download.opensuse.org/repositories/home:/smarty12:/libraries/openSUSE_Leap_15.0/x86_64/libdvdcss2-1.4.0+git2.2f12236-lp150.2.2.x86_64.rpm
+rpm -i libdvdcss2-1.4.0+git2.2f12236-lp150.2.2.x86_64.rpm
+rm -rf ibdvdcss2-1.4.0+git2.2f12236-lp150.2.2.x86_64.rpm
+zypper install -y libdvdnav4 libdvdread4 ffmpeg lame gstreamer gstreamer-plugins-libav gstreamer-plugins-good gstreamer-plugins-bad gstreamer-plugins-ugly redshift qbittorrent vlc vlc-codec-gstreamer simplescreenrecorder kdenlive firewall-config
 
 # Adobe Flash.
-zypper ar --check --refresh http://linuxdownload.adobe.com/linux/x86_64/ adobe
-echo Type 'yes' then press the enter key
-zypper ref
-zypper in adobe-release-x86_64 flash-plugin
+rpm -ivh http://linuxdownload.adobe.com/adobe-release/adobe-release-x86_64-1.0-1.noarch.rpm
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
 zypper ref
+zypper in -y adobe-release-x86_64 flash-plugin
 
 # Install neofetch and run it.
 git clone https://github.com/dylanaraps/neofetch.git
