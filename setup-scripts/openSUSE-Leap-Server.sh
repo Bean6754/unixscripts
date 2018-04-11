a5abe23ed3da0e4add9fcc35987c3df1800487b1
Matthew <Bean6754@users.noreply.github.com>
2018-04-11 02:56:23 +0100

    Update openSUSE-Leap-Server.sh

--- setup-scripts/openSUSE-Leap-Server.sh
+++ setup-scripts/openSUSE-Leap-Server.sh
@@ -13,7 +13,7 @@ zypper dup -y
 
 # Install development tools.
 zypper install -t pattern devel_basis
-zypper install -y make cmake git curl wget lsof strace htop vim emacs-nox python2 python3 ruby gcc gcc-c++ lua tcl java-11-openjdk java-11-openjdk-devel qbittorrent-nox
+zypper install -y lsb-release make cmake git curl wget lsof strace htop vim emacs-nox python2 python3 ruby gcc gcc-c++ lua tcl java-11-openjdk java-11-openjdk-devel qbittorrent-nox
 
 # Networking tools.
 zypper in -y wireshark tcpdump
