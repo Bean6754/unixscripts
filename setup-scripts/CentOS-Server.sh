51bda34fe7d80b6d3e39f54419bbc54a8eaab7a8
Matthew <Bean6754@users.noreply.github.com>
2018-04-14 20:51:41 +0100

    Update and rename CentOS-7-Server-Setup.sh to CentOS-Server.sh

--- setup-scripts/CentOS-Server.sh
+++ setup-scripts/CentOS-Server.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 # Make sure only root can run our script
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
@@ -15,8 +11,8 @@ yum update -y
 # Install stuff.
 yum install -y epel-release
 yum update -y
-yum install -y dnf microcode_ctl kernel-devel zip unzip p7zip p7zip-plugins git wget curl htop strace lsof nc tcpdump vim emacs-nox nethogs iftop
 yum groupinstall -y "Development Tools"
+yum install -y dnf microcode_ctl kernel-devel zip unzip p7zip p7zip-plugins git wget curl htop strace lsof nc tcpdump vim emacs-nox nethogs iftop
 
 # Install neofetch and run it.
 git clone https://github.com/dylanaraps/neofetch
