cdfab876b390c825379eacc6b6907ed213e43536
Matthew <Bean6754@users.noreply.github.com>
2018-04-18 23:53:40 +0100

    Update and rename Fedora-27-LXQt.sh to Fedora-27-Desktop.sh

--- setup-scripts/Fedora-27-Desktop.sh
+++ setup-scripts/Fedora-27-Desktop.sh
@@ -25,8 +25,8 @@ dnf install -y chkrootkit clamav clamav-update
 freshclam
 # High level.
 # KDE: dnf install -y setroubleshoot qt5ct qt5-qtconfiguration libreoffice libreoffice-langpack-en gimp inkscape inkscape-psd krita transmission-qt pavucontrol-qt wireshark-qt steam vulkan vulkan.i686 qt-creator kde-partitionmanager quassel kdenlive simplescreenrecorder filezilla redshift plasma-applet-redshift-control
-# dnf install -y setroubleshoot libreoffice libreoffice-langpack-en gnome-tweak-tool gimp transmission pavucontrol wireshark-gtk steam vulkan vulkan.i686 gnome-builder geany glade gparted guvcview polari pitivi simplescreenrecorder filezilla redshift redshift-gtk conky conky-manager
-dnf install -y setroubleshoot chromium libreoffice libreoffice-langpack-en gimp transmission-qt wireshark-qt steam vulkan vulkan.i686 qt-creator kde-partitionmanager guvcview quassel simplescreenrecorder filezilla redshift conky conky-manager
+dnf install -y setroubleshoot chromium kdenlive libreoffice libreoffice-langpack-en gnome-tweak-tool gimp transmission pavucontrol wireshark-gtk steam vulkan vulkan.i686 gnome-builder geany glade gparted guvcview polari pitivi simplescreenrecorder filezilla redshift redshift-gtk conky conky-manager
+# LXQt: dnf install -y setroubleshoot chromium libreoffice libreoffice-langpack-en gimp transmission-qt wireshark-qt steam vulkan vulkan.i686 qt-creator kde-partitionmanager guvcview quassel simplescreenrecorder filezilla redshift conky conky-manager
 # XFCE: dnf install -y setroubleshoot qt5ct qt5-qtconfiguration libreoffice libreoffice-langpack-en gimp transmission-gtk pavucontrol wireshark-gtk steam vulkan vulkan.i686 geany glade gparted guvcview kdenlive simplescreenrecorder filezilla redshift redshift-gtk conky conky-manager wmctrl
 setsebool -P selinuxuser_execheap 1
 # XFCE: dnf remove -y leafpad ; dnf install -y mousepad
@@ -41,10 +41,11 @@ setsebool -P selinuxuser_execheap 1
 # Fonts.
 dnf install -y liberation-fonts-common dejavu-fonts-common google-noto-fonts-common google-noto-emoji-fonts google-noto-mono-fonts google-noto-cjk-fonts-common google-noto-mono-fonts google-noto-sans-fonts google-noto-serif-fonts
 # Themes.
-dnf install -y plasma-breeze sddm-breeze sddm-themes
+# KDE/LXQt: dnf install -y plasma-breeze sddm-breeze sddm-themes
 # XFCE: dnf install -y adwaita-cursor-theme adwaita-gtk2-theme adwaita-icon-theme gtk-murrine-engine numix-gtk-theme numix-icon-theme numix-icon-theme-circle paper-icon-theme tango-icon-theme tango-icon-theme-extras faience-icon-theme
-# GNOME: dnf install -y adwaita-cursor-theme adwaita-icon-theme numix-gtk-theme numix-icon-theme numix-icon-theme-circle paper-icon-theme tango-icon-theme tango-icon-theme-extras faience-icon-theme
+dnf install -y adwaita-cursor-theme adwaita-icon-theme numix-gtk-theme numix-icon-theme numix-icon-theme-circle paper-icon-theme tango-icon-theme tango-icon-theme-extras faience-icon-theme
 # Google Chrome (for Adobe Flash, WebRTC, etc.. support)
+dnf remove -y firefox && dnf autoremove -y # soz.
 curl -LO https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 dnf install -y google-chrome-stable_current_x86_64.rpm
 # rm -rf google-chrome-stable_current_x86_64.rpm - not really needed and would be useful to keep package backed-up.
