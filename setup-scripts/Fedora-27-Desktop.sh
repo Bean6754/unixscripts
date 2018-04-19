9f39574aa137512ba580fc1eebaedc6e03f51f85
Matthew <Bean6754@users.noreply.github.com>
2018-04-19 10:57:08 +0100

    Update Fedora-27-Desktop.sh

--- setup-scripts/Fedora-27-Desktop.sh
+++ setup-scripts/Fedora-27-Desktop.sh
@@ -17,7 +17,7 @@ dnf update -y
 # Group packages.
 dnf groupinstall -y "Development-Tools"
 # Low level.
-dnf install -y kernel-devel kernel-headers acpid dkms strace lsof htop git curl wget vim emacs-nox tmux transmission-cli gcc-c++ ruby nmap p7zip p7zip-plugins zip unzip tftp wireshark wireshark-cli java-1.8.0-openjdk java-1.8.0-openjdk-devel neofetch
+dnf install -y kernel-devel kernel-headers acpid dkms strace lsof htop git curl wget vim emacs-nox tmux transmission-cli gcc-c++ ruby nmap p7zip p7zip-plugins zip unzip tftp wireshark-cli java-1.8.0-openjdk java-1.8.0-openjdk-devel neofetch
 # Network monitoring tools.
 dnf install -y nethogs iftop
 # Security.
