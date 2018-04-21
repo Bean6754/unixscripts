a1aaa1cae6e650f4145eaa6653edffeb04fd5045
Matthew <Bean6754@users.noreply.github.com>
2018-04-21 01:13:59 +0100

    Update mac-ports.sh

--- setup-scripts/mac-ports.sh
+++ setup-scripts/mac-ports.sh
@@ -29,7 +29,7 @@ sudo $port -v selfupdate
 # To view outdated packages: sudo $port -v outdated
 sudo $port -v upgrade outdated
 # Install software.
-sudo $port -v install arping arp-scan autoconf automake clamav dsniff ettercap iftop htop libplist lua neofetch nmap python27 python36 py27-pip py36-pip rsync screen tcsh texlive tmux vim
+sudo $port -v install arping arp-scan autoconf automake bash clamav coreutils dsniff ettercap iftop htop libplist lua neofetch nmap python27 python36 py27-pip py36-pip rsync screen tcsh texlive tmux vim
 # Get mpv, iTerm2 and transmission online, they selfupdate.
 # Setup python2 and python3.
 sudo $port select --set python2 python27
