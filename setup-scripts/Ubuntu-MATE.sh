2d8fa355740bddcb31dda39df1ad5bbbe0113a55
Matthew <Bean6754@users.noreply.github.com>
2018-04-23 22:21:47 +0100

    Update Ubuntu-MATE.sh
    Add 'kdenlive'.

--- setup-scripts/Ubuntu-MATE.sh
+++ setup-scripts/Ubuntu-MATE.sh
@@ -15,7 +15,7 @@ apt full-upgrade -y
 # Low-Level.
 apt install -y aptitude wget curl git strace lsof htop vim emacs-nox zip unzip p7zip-full build-essential default-jdk tshark tcpdump nmap transmission-cli hddtemp lm-sensors neofetch scanmem tmux
 # High-Level.
-apt install -y gimp vlc wireshark-gtk transmission-gtk gparted geany geany-plugins glade libbluray2 libdvdcss2 libdvdnav4 libdvdread4 ubuntu-restricted-extras ubuntu-restricted-addons gameconqueror kdenlive libreoffice libreoffice-l10n-en-gb pavucontrol redshift redshift-gtk simplescreenrecorder virtualbox virtualbox-ext-pack virtualbox-guest-additions-iso
+apt install -y gimp vlc wireshark-gtk transmission-gtk gparted geany geany-plugins glade libbluray2 libdvdcss2 libdvdnav4 libdvdread4 ubuntu-restricted-extras ubuntu-restricted-addons gameconqueror kdenlive libreoffice libreoffice-l10n-en-gb pavucontrol redshift redshift-gtk simplescreenrecorder kdenlive virtualbox virtualbox-ext-pack virtualbox-guest-additions-iso
 dpkg-reconfigure libdvd-pkg
 # Themes.
 apt install -y chameleon-cursor-theme moka-icon-theme qt5ct
