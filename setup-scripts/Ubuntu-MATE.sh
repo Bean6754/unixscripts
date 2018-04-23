5e340ddcd97e2414049ed93281d485b91530fc31
Matthew <Bean6754@users.noreply.github.com>
2018-04-23 23:57:12 +0100

    Update Ubuntu-MATE.sh

--- setup-scripts/Ubuntu-MATE.sh
+++ setup-scripts/Ubuntu-MATE.sh
@@ -18,4 +18,4 @@ apt install -y aptitude wget curl git strace lsof htop vim emacs-nox zip unzip p
 apt install -y bleachbit gimp vlc wireshark-gtk transmission-gtk gparted geany geany-plugins glade libbluray2 libdvdcss2 libdvdnav4 libdvdread4 ubuntu-restricted-extras ubuntu-restricted-addons gameconqueror kdenlive libreoffice libreoffice-l10n-en-gb pavucontrol redshift redshift-gtk simplescreenrecorder kdenlive virtualbox virtualbox-ext-pack virtualbox-guest-additions-iso
 dpkg-reconfigure libdvd-pkg
 # Themes.
-apt install -y arc-theme oxygen-cursor-theme moka-icon-theme # Somehow MATE will automatically do qt5ct stuff. 0.o
+apt install -y arc-theme compiz compizconfig-settings-manager oxygen-cursor-theme moka-icon-theme # Somehow MATE will automatically do qt5ct stuff. 0.o
