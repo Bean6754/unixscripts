4e1987ef252992fec5a5636bfa16207f2d1a82e0
Matthew <Bean6754@users.noreply.github.com>
2018-04-25 12:48:12 +0100

    Create nestfolderflood.sh

---
+++ fun/nestfolderflood.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ~
+mkdir lol
+cd lol
+
+while(true)
+do
+  mkdir $[ $RANDOM % 999999999 ]
+  cd $[ $RANDOM % 999999999 ]
+  cd $[ $RANDOM % 999999999 ]
+  cd $[ $RANDOM % 999999999 ]
+  cd $[ $RANDOM % 999999999 ]
+  
+  cd ~/lol
+done
