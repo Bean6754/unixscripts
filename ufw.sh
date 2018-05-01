708b96ca290acbce5790a280f86ec6db8fd90dca
Matthew <Bean6754@users.noreply.github.com>
2018-05-01 10:02:56 +0100

    Update ufw.sh

--- ufw.sh
+++ ufw.sh
@@ -1,10 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
 # Make sure only root can run our script
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
