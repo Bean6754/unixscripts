3cf2ae43c088ed336126bbfd72fea3e239a1b0eb
Matthew <Bean6754@users.noreply.github.com>
2018-05-05 17:27:21 +0100

    Create ufw.sh

---
+++ server-tweaks/ufw.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# Enable then start service at boot.
+systemctl enable ufw
+systemctl start ufw
+
+# Enable firewall.
+ufw enable
+
+# Default rules.
+ufw default deny incoming
+ufw default allow outgoing
+
+# SSH.
+ufw allow 22/tcp
+# HTTP.
+ufw allow 80/tcp
+ufw allow 8080/tcp
+# HTTPS.
+ufw allow 443/tcp
+# Samba.
+ufw allow 139/tcp
+ufw allow 445/tcp
+ufw allow 137/udp
+ufw allow 138/udp
+
+# Reload firewall.
+ufw reload
