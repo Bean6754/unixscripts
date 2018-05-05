75adbef5dec8a68b065f54cc1ccb56726336afb3
Matthew <Bean6754@users.noreply.github.com>
2018-05-05 15:06:05 +0100

    Update startup-decrypt.sh

--- server-tweaks/startup-decrypt.sh
+++ server-tweaks/startup-decrypt.sh
@@ -3,11 +3,8 @@
 start() {
 	PASSWD="password"
 	
-	vgscan --mknodes
-	vgchange -ay
-	
-	echo -n $PASSWD | cryptsetup luksOpen /dev/mapper/vg01-storage1 storage1_crypt -d -
-	echo -n $PASSWD | cryptsetup luksOpen /dev/mapper/vg02-storage2 storage2_crypt -d -
+	echo -n $PASSWD | cryptsetup luksOpen /dev/sda1 storage1_crypt -d -
+	echo -n $PASSWD | cryptsetup luksOpen /dev/sdb1 storage2_crypt -d -
 	
 	# Make folders (just in case).
 	mkdir -p /mnt/storage1
@@ -15,6 +12,20 @@ start() {
 	
 	mount /dev/mapper/storage1_crypt /mnt/storage1
 	mount /dev/mapper/storage2_crypt /mnt/storage2
+	
+	# LVM (Not recommended, not very hot-swappable).
+	# vgscan --mknodes
+	# vgchange -ay
+	
+	# echo -n $PASSWD | cryptsetup luksOpen /dev/mapper/vg01-storage1 storage1_crypt -d -
+	# echo -n $PASSWD | cryptsetup luksOpen /dev/mapper/vg02-storage2 storage2_crypt -d -
+	
+	# Make folders (just in case).
+	# mkdir -p /mnt/storage1
+	# mkdir -p /mnt/storage2
+	
+	# mount /dev/mapper/storage1_crypt /mnt/storage1
+	# mount /dev/mapper/storage2_crypt /mnt/storage2
 	
 	systemctl restart httpd
 	#systemctl restart nginx
@@ -27,8 +38,9 @@ stop() {
 	cryptsetup luksClose storage1_crypt
 	cryptsetup luksClose storage2_crypt
 	
-	lvchange -an -v /dev/vg01/storage1
-	lvchange -an -v /dev/vg02/storage2
+	# LVM (Not recommended, not very hot-swappable).
+	# lvchange -an -v /dev/vg01/storage1
+	# lvchange -an -v /dev/vg02/storage2
 	
 	systemctl stop httpd
 	#systemctl stop nginx
