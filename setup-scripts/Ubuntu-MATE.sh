c060b042c6fdaf1fa1e2d3031dc9987e2a738a96
Matthew <Bean6754@users.noreply.github.com>
2018-05-05 14:34:52 +0100

    Update Ubuntu-MATE.sh

--- setup-scripts/Ubuntu-MATE.sh
+++ setup-scripts/Ubuntu-MATE.sh
@@ -13,7 +13,7 @@ apt dist-upgrade -y
 apt full-upgrade -y
 
 # Low-Level.
-apt install -y aptitude wget curl git strace lsof htop vim emacs-nox zip unzip p7zip-full build-essential default-jdk tshark tcpdump nmap transmission-cli hddtemp lm-sensors neofetch scanmem tmux
+apt install -y aptitude wget curl git strace lsof htop vim emacs-nox zip unzip p7zip-full build-essential default-jdk tshark tcpdump nmap transmission-cli hddtemp lm-sensors neofetch scanmem tmux ufw
 # High-Level.
 apt install -y bleachbit gimp vlc wireshark-gtk transmission-gtk gparted geany geany-plugins glade libbluray2 libdvdcss2 libdvdnav4 libdvdread4 ubuntu-restricted-extras ubuntu-restricted-addons gameconqueror kdenlive libreoffice libreoffice-l10n-en-gb pavucontrol redshift redshift-gtk simplescreenrecorder kdenlive virtualbox virtualbox-ext-pack virtualbox-guest-additions-iso
 dpkg-reconfigure libdvd-pkg
