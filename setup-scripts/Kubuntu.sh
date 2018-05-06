ab3fc5bb172f7818646d64512839a42b87382d28
Matthew <Bean6754@users.noreply.github.com>
2018-05-06 23:59:02 +0100

    Update Kubuntu.sh

--- setup-scripts/Kubuntu.sh
+++ setup-scripts/Kubuntu.sh
@@ -17,7 +17,7 @@ apt full-upgrade -y
 # Low-Level.
 apt install -y aptitude wget curl git strace lsof htop vim emacs-nox zip unzip p7zip-full build-essential default-jdk tshark tcpdump nmap qbittorrent-nox hddtemp lm-sensors neofetch scanmem tmux ufw
 # High-Level.
-apt install -y bleachbit krita inkscape inkscape-open-symbols vlc wireshark-qt qbittorent partitionmanager qtcreator libbluray2 libdvdcss2 libdvdnav4 libdvdread4 kubuntu-restricted-extras kubuntu-restricted-addons gameconqueror kdenlive libreoffice libreoffice-l10n-en-gb pavucontrol-qt redshift plasma-applet-redshift-control simplescreenrecorder kdenlive virtualbox virtualbox-ext-pack virtualbox-guest-additions-iso
+apt install -y bleachbit krita inkscape inkscape-open-symbols vlc wireshark-qt qbittorrent partitionmanager qtcreator libbluray2 libdvdcss2 libdvdnav4 libdvdread4 kubuntu-restricted-extras kubuntu-restricted-addons gameconqueror kdenlive libreoffice libreoffice-l10n-en-gb pavucontrol-qt redshift plasma-applet-redshift-control simplescreenrecorder kdenlive virtualbox virtualbox-ext-pack virtualbox-guest-additions-iso
 dpkg-reconfigure libdvd-pkg
 # Atom.
 curl -LO https://atom.io/download/deb
