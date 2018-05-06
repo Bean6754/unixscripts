297b3a6d378f0f96f9ebb164b0af138dc018dd57
Matthew <Bean6754@users.noreply.github.com>
2018-05-06 23:58:09 +0100

    Update Ubuntu-MATE.sh

--- setup-scripts/Ubuntu-MATE.sh
+++ setup-scripts/Ubuntu-MATE.sh
@@ -13,9 +13,15 @@ apt dist-upgrade -y
 apt full-upgrade -y
 
 # Low-Level.
-apt install -y aptitude wget curl git strace lsof htop vim emacs-nox zip unzip p7zip-full build-essential default-jdk tshark tcpdump nmap transmission-cli hddtemp lm-sensors neofetch scanmem tmux ufw
+apt install -y aptitude wget curl git strace lsof htop vim emacs-nox zip unzip p7zip-full build-essential default-jdk tshark tcpdump nmap qbittorrent-nox hddtemp lm-sensors neofetch scanmem tmux ufw
 # High-Level.
-apt install -y bleachbit gimp vlc wireshark-gtk transmission-gtk gparted geany geany-plugins glade libbluray2 libdvdcss2 libdvdnav4 libdvdread4 ubuntu-restricted-extras ubuntu-restricted-addons gameconqueror kdenlive libreoffice libreoffice-l10n-en-gb pavucontrol redshift redshift-gtk simplescreenrecorder kdenlive virtualbox virtualbox-ext-pack virtualbox-guest-additions-iso
+apt install -y bleachbit krita inkscape inkscape-open-symbols vlc wireshark-qt qbittorent partitionmanager qtcreator libbluray2 libdvdcss2 libdvdnav4 libdvdread4 kubuntu-restricted-extras kubuntu-restricted-addons gameconqueror kdenlive libreoffice libreoffice-l10n-en-gb pavucontrol-qt redshift plasma-applet-redshift-control simplescreenrecorder kdenlive virtualbox virtualbox-ext-pack virtualbox-guest-additions-iso
 dpkg-reconfigure libdvd-pkg
+# Atom.
+curl -LO https://atom.io/download/deb
+mv deb atom-amd64.deb
+dpkg -i atom-amd64.deb
+apt install -f
+rm -rf atom-amd64.deb
 # Themes.
-apt install -y arc-theme compiz compizconfig-settings-manager oxygen-cursor-theme moka-icon-theme # Somehow MATE will automatically do qt5ct stuff. 0.o
+apt install -y arc-theme oxygen-cursor-theme moka-icon-theme
