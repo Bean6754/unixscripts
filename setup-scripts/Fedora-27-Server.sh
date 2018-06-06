0c76fc8199a612b028a880e006f81807fb3b8bf1
Matthew <Bean6754@users.noreply.github.com>
2018-06-06 01:20:36 +0100

    Update Fedora-27-Server.sh

--- setup-scripts/Fedora-27-Server.sh
+++ setup-scripts/Fedora-27-Server.sh
@@ -15,7 +15,7 @@ dnf update -y
 # Group packages.
 dnf groupinstall -y "Development Tools" "C Development Tools and Libraries"
 # Low level.
-dnf install -y kernel-devel kernel-headers acpid dkms strace lsof htop git curl wget vim emacs-nox tmux transmission-cli gcc-c++ ruby nmap p7zip p7zip-plugins zip unzip tftp wireshark-cli java-1.8.0-openjdk java-1.8.0-openjdk-devel neofetch figlet toilet cowsay
+dnf install -y kernel-devel kernel-headers acpid dkms strace lsof htop git curl wget vim emacs-nox tmux transmission-cli gcc-c++ ruby nmap p7zip p7zip-plugins zip unzip tftp wireshark-cli java-1.8.0-openjdk java-1.8.0-openjdk-devel neofetch figlet toilet cowsay cmatrix
 # Network monitoring tools.
 dnf install -y nethogs iftop
 # Security.
