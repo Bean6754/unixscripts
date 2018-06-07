27c091fda3498dcabbf9ac7a129be2105e0a4f74
Matthew <Bean6754@users.noreply.github.com>
2018-06-07 13:41:41 +0100

    Update LinuxMint-Tara.sh

--- setup-scripts/LinuxMint-Tara.sh
+++ setup-scripts/LinuxMint-Tara.sh
@@ -16,7 +16,7 @@ apt full-upgrade -y
 apt autoremove -y
 
 # Low-level.
-apt install -y aptitude wget curl git strace lsof htop vim emacs-nox zip unzip p7zip-full build-essential default-jdk transmission-cli hddtemp lm-sensors neofetch scanmem tmux
+apt install -y aptitude wget curl git strace lsof htop vim emacs-nox zip unzip p7zip-full build-essential default-jdk transmission-cli hddtemp lm-sensors neofetch scanmem tmux youtube-dl
 # Fun.
 apt install -y figlet toilet lolcat cowsay cowsay-off
 # Networking tools.
@@ -24,7 +24,7 @@ apt install -y tshark tcpdump nmap ufw net-tools ssh iptables iptables-persisten
 systemctl enable netfilter-persistent
 systemctl start netfilter-persistent
 # High-level.
-apt install -y bleachbit gimp mpv vlc steam wine32-development wine64-development wireshark-gtk gufw transmission transmission-gtk gparted baobab glade geany geany-plugins libbluray2 libdvdcss2 libdvdnav4 libdvdread4 ubuntu-restricted-extras ubuntu-restricted-addons mint-meta-codecs gameconqueror kdenlive libreoffice libreoffice-l10n-en-gb pavucontrol redshift redshift-gtk simplescreenrecorder guvcview kdenlive virtualbox virtualbox-qt virtualbox-ext-pack virtualbox-guest-additions-iso skypeforlinux
+apt install -y bleachbit gimp vlc steam wine32-development wine64-development wireshark-gtk gufw transmission transmission-gtk gparted baobab glade geany geany-plugins libbluray2 libdvdcss2 libdvdnav4 libdvdread4 ubuntu-restricted-extras ubuntu-restricted-addons mint-meta-codecs gameconqueror kdenlive libreoffice libreoffice-l10n-en-gb pavucontrol redshift redshift-gtk simplescreenrecorder guvcview kdenlive virtualbox virtualbox-qt virtualbox-ext-pack virtualbox-guest-additions-iso skypeforlinux
 apt install -y playonlinux # Install after 'wine32-development' and 'wine64-development', just in case.
 apt install -y chromium pepperflashplugin-nonfree
 apt purge -y firefox && apt autoremove -y
