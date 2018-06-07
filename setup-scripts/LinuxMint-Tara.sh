f3845d0229ed5f4055f52965c74034c46906187d
Matthew <Bean6754@users.noreply.github.com>
2018-06-07 13:50:28 +0100

    Update LinuxMint-Tara.sh

--- setup-scripts/LinuxMint-Tara.sh
+++ setup-scripts/LinuxMint-Tara.sh
@@ -26,7 +26,7 @@ systemctl start netfilter-persistent
 # High-level.
 apt install -y bleachbit gimp vlc steam wine32-development wine64-development wireshark-gtk gufw transmission transmission-gtk gparted baobab glade geany geany-plugins libbluray2 libdvdcss2 libdvdnav4 libdvdread4 ubuntu-restricted-extras ubuntu-restricted-addons mint-meta-codecs gameconqueror kdenlive libreoffice libreoffice-l10n-en-gb pavucontrol redshift redshift-gtk simplescreenrecorder guvcview kdenlive virtualbox virtualbox-qt virtualbox-ext-pack virtualbox-guest-additions-iso skypeforlinux
 apt install -y playonlinux # Install after 'wine32-development' and 'wine64-development', just in case.
-apt install -y chromium pepperflashplugin-nonfree
+apt install -y chromium-browser pepperflashplugin-nonfree
 apt purge -y firefox && apt autoremove -y
 dpkg-reconfigure libdvd-pkg
 # Themes.
