1984b58d2f7541b2b397f00021bb4e279be5d27f
Matthew <Bean6754@users.noreply.github.com>
2018-06-13 23:42:48 +0100

    Update rpi.sh

--- setup-scripts/rpi.sh
+++ setup-scripts/rpi.sh
@@ -6,6 +6,13 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+## NOTE: This is a very personal setup, just like all of my other scripts.
+
+# Personal instructions.
+# 1. Setup static IP though `/etc/dhcpcd.conf`.
+# 2. Install packages.
+# 3. Setup lighttpd config.
+
 # Low-level.
 apt install -y aptitude neofetch git curl wget vim emacs-nox sudo fakeroot p7zip-full zip unzip scanmem strace lsof htop screen tmux nmap build-essential ruby tshark lua5.3 gdisk tftp ftp tcpdump transmission-cli transmission-daemon net-tools nethogs iftop software-properties-common ntp exif imagemagick lm-sensors hddtemp
 
