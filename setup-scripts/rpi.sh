65a6428de11ca15bf17bab4187d6e39a6e16b83f
Matthew <Bean6754@users.noreply.github.com>
2018-06-13 21:04:32 +0100

    Create rpi.sh

---
+++ setup-scripts/rpi.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# Low-level.
+apt install -y aptitude neofetch git curl wget vim emacs-nox sudo fakeroot p7zip-full zip unzip rar unrar scanmem strace lsof htop screen tmux nmap build-essential ruby tshark lua5.3 gdisk tftp ftp tcpdump transmission-cli transmission-daemon net-tools nethogs iftop software-properties-common ntp exif imagemagick lm-sensors hddtemp
+
+# Security.
+apt install -y clamav chkrootkit rkhunter lynis
+freshclam
+
+# Server specific.
+apt install -y lighttpd mariadb-server php7.0-fpm php7.0-mysql
+
+systemctl enable lighttpd
+systemctl start lighttpd
+
+lighttpd-enable-mod fastcgi
+lighttpd-enable-mod fastcgi-php
