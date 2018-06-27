c29c793ff9951867b8d6781a7bb9202d9e2fd333
Matthew <Bean6754@users.noreply.github.com>
2018-06-27 17:36:28 +0100

    Create gnome-mem-fix.sh

---
+++ Security/gnome-mem-fix.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This is a temporary fix for the GNOME 3 memory leak.
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+while (true)
+do
+  killall -3 gnome-shell
+  sleep 20
+done
