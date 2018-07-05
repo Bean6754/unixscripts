3bad1ce700f1462409376b82c89ae8ec7b45215c
Matthew <Bean6754@users.noreply.github.com>
2018-07-05 19:29:43 +0100

    Update rpi.sh

--- setup-scripts/rpi.sh
+++ setup-scripts/rpi.sh
@@ -11,7 +11,7 @@ fi
 # Personal instructions.
 # 1. Setup static IP though `/etc/dhcpcd.conf`.
 # 2. Install packages.
-# 3. Setup lighttpd config.
+# 3. Setup apache2 config.
 
 # Low-level.
 apt install -y aptitude neofetch git curl wget vim emacs-nox sudo fakeroot p7zip-full zip unzip scanmem strace lsof htop screen tmux nmap build-essential ruby tshark lua5.3 gdisk tftp ftp tcpdump qbittorrent-nox net-tools nethogs iftop software-properties-common ntp exif imagemagick lm-sensors hddtemp tree
