e4b5cf8aee5ed0dbeba02c5cf05ba1dfb9b58b9b
Matthew <Bean6754@users.noreply.github.com>
2018-08-02 21:26:38 +0100

    Create advanced_countdown.sh

---
+++ fun/advanced_countdown.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+while true
+do
+	clear
+	echo ls -aGl
+	echo
+	
+	secs=$((60))
+	while [ $secs -gt 0 ]
+	do
+		echo -ne "Waiting $secs second(s) \033[0K\r"
+		sleep 1
+		: $((secs--))
+	done
+done
