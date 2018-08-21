0faf3c9f2ec3d42c8fb8c171dcc4593ca4956f77
Matthew <Bean6754@users.noreply.github.com>
2018-08-22 00:41:39 +0100

    Update Solus-Desktop.sh

--- setup-scripts/Solus-Desktop.sh
+++ setup-scripts/Solus-Desktop.sh
@@ -7,4 +7,5 @@ eopkg up -y
 eopkg it -y -c system.devel
 eopkg it -y emacs neofetch nmap tcpdump vim wine wine-32bit
 # nvidia-glx-driver-current
-eopkg it -y audacity gimp lutris pavucontrol putty steam virtualbox-current wireshark
+eopkg it -y audacity gimp lutris pavucontrol putty redshift steam virtualbox-current wireshark
+eopkg it -y lugaru
