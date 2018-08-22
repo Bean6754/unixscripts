4b55bb305b70a106c53102023c099fa8691c1dc1
Matthew <Bean6754@users.noreply.github.com>
2018-08-22 01:14:27 +0100

    Update Solus-Desktop.sh

--- setup-scripts/Solus-Desktop.sh
+++ setup-scripts/Solus-Desktop.sh
@@ -9,6 +9,6 @@ eopkg up -y
 eopkg it -y -c system.devel
 eopkg it -y cmus curl emacs git htop lsof neofetch nmap strace tcpdump vim wget wine wine-32bit
 # nvidia-glx-driver-current
-eopkg it -y audacity deadbeef gimp lutris pavucontrol putty redshift steam virtualbox-current mpv wireshark
+eopkg it -y audacity gimp lutris pavucontrol putty redshift steam virtualbox-current mpv wireshark
 eopkg it -y libbluray libdvdcss libdvdnav libdvdread
 eopkg dc
