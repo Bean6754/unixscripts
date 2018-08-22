ef9d3e764c4bfc65e7fb600765318b6a24068b99
Matthew <Bean6754@users.noreply.github.com>
2018-08-22 01:24:28 +0100

    Update Solus-Desktop.sh

--- setup-scripts/Solus-Desktop.sh
+++ setup-scripts/Solus-Desktop.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 # dc = delete-cache
 # up = upgrade
 # it = install
@@ -10,5 +16,11 @@ eopkg it -y -c system.devel
 eopkg it -y cmus curl emacs git htop lsof neofetch nmap strace tcpdump vim wget wine wine-32bit
 # nvidia-glx-driver-current
 eopkg it -y audacity gimp lutris pavucontrol putty redshift steam virtualbox-current mpv wireshark
+# Codecs.
 eopkg it -y libbluray libdvdcss libdvdnav libdvdread
+# Google Chrome.
+eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/web/browser/google-chrome-stable/pspec.xml
+eopkg it google-chrome-*.eopkg
+rm google-chrome-*.eopkg
+
 eopkg dc
