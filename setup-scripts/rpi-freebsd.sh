67d5258efecbee4ab4ed5a5b8e5a1891c15bbe5d
Matthew <Bean6754@users.noreply.github.com>
2018-09-01 12:00:52 +0100

    Update rpi-freebsd.sh

--- setup-scripts/rpi-freebsd.sh
+++ setup-scripts/rpi-freebsd.sh
@@ -11,6 +11,8 @@ echo "# Custom." >> /etc/rc.conf
 # Check date and time first!
 # https://www.cyberciti.biz/tips/freebsd-timeclock-synchronization-with-ntp-server.html
 ntpdate -s time.nist.gov
+echo >> /etc/rc.conf
+echo "# NTP." >> /etc/rc.conf
 echo 'ntpdate_enable="YES"' >> /etc/rc.conf
 echo 'ntpdate_hosts="time.nist.gov"' >> /etc/rc.conf
 
@@ -33,8 +35,16 @@ pkg install -y nmap tshark
 # To download free GeoIP databases.
 /usr/local/bin/geoipupdate.sh
 # Server.
-pkg install -y nginx
+# https://www.digitalocean.com/community/tutorials/how-to-install-an-nginx-mysql-and-php-femp-stack-on-freebsd-10-1
+pkg install -y mariadb102-server nginx php72 php72-mysqli
+echo >> /etc/rc.conf
+echo '# Web server.' >> /etc/rc.conf
+echo 'mysql_enable="YES"' >> /etc/rc.conf
 echo 'nginx_enable="YES"' >> /etc/rc.conf
+echo 'php_fpm_enable="YES"' >> /etc/rc.conf
+service mysql-server onestart
+mysql_secure_installation
+
 
 # Linux emulation not availible for aarch64 (RPi).
 # echo 'proc /proc procfs rw,noauto 0 0' >> /etc/fstab
