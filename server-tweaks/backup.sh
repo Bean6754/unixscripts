59c3713bb02f38ca249116c0bed6b02f71f2280c
Matthew <Bean6754@users.noreply.github.com>
2018-09-07 18:07:22 +0100

    Update backup.sh

--- server-tweaks/backup.sh
+++ server-tweaks/backup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# Variables.
 shortdate=`date +"%d-%m-%y"`
 
 mkdir -p /mnt/f/backup_$shortdate
