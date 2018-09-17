ad1bc9392b965c98f9dda2ad280bebe403828cc8
Matthew <Bean6754@users.noreply.github.com>
2018-09-17 13:59:49 +0100

    Update find.sh

--- fun/find.sh
+++ fun/find.sh
@@ -7,7 +7,7 @@ if [ "$1" = "" ] || [ "$2" = "" ]; then
 	echo "find.sh <directory> <string>"
 	exit 1
 elif [ "$1" != "" ] && [ "$2" != "" ]; then
-	find $DIR -iname "*$STRING*"
+	find $DIR -iname "*$STRING*" 2>/dev/null
 else
 	exit 1
 fi
