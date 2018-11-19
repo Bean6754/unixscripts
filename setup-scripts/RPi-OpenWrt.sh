952a0b39e91bb4dd65d5cabd9735eaab60c09b51
Matthew <Bean6754@users.noreply.github.com>
2018-11-19 22:01:16 +0000

    Update RPi-OpenWrt.sh

--- setup-scripts/RPi-OpenWrt.sh
+++ setup-scripts/RPi-OpenWrt.sh
@@ -3,7 +3,6 @@
 # uci show network
 # uci set network.lan.ipaddr=192.168.1.4
 # uci set network.lan.gateway=192.168.1.254
-# uci set network.lan.broadcast=192.168.1.255
 # uci set network.lan.dns=192.168.1.254
 # uci commit
 # /etc/init.d/network restart
