c87572f683598b88abe2992ca37a3c869b0025f9
Matthew <Bean6754@users.noreply.github.com>
2018-12-13 17:54:31 +0000

    Update setup.sh

--- Mac/setup.sh
+++ Mac/setup.sh
@@ -17,7 +17,7 @@ echo 'export PATH="$PATH:/usr/local/bin:/usr/local/sbin"' >> ~/.zshrc
 # Fix Vim backspace issue.
 echo "set backspace=2" >> ~/.vimrc
 
-brew install curl wget bash emacs nano git openssh perl rsync svn unzip vim zsh
+brew install curl wget bash emacs nano git openssh perl rsync svn unzip vim
 
 # Setup bash for local user.
 sudo bash -c 'cat >> /etc/shells << EOF
@@ -27,7 +27,7 @@ EOF'
 chsh -s /usr/local/bin/bash
 
 # git curl wget python, already installed from Xcode utils.
-brew install arping arpoison arp-scan bash-completion cmus ettercap htop iftop lua neofetch nmap nethogs telnet tmux tor tree zsh
+brew install arping arpoison arp-scan bash-completion cmus ettercap htop iftop lua neofetch nmap nethogs telnet tmux tor tree mc youtube-dl
 brew link bash-completion
 # Fun.
 brew install cowsay figlet lolcat toilet
