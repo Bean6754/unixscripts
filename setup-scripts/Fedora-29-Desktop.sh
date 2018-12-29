a565286c868bf7a6a4ad7ba16715251e70354a14
Matthew <Bean6754@users.noreply.github.com>
2018-12-29 11:30:05 +0000

    Create Fedora-29-Desktop.sh

---
+++ setup-scripts/Fedora-29-Desktop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## UNCOMPLETE!
+
+# Add RPMFusion.
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# Update system.
+sudo dnf update -y
+
+# Group packages.
+dnf groupinstall -y "Development Tools" "C Development Tools and Libraries" "Security Lab"
+
+sudo dnf install -y chromium vlc vlc-extras wireshark wireshark-cli nmap
+
+# Themes.
+sudo dnf install -y gnome-tweaks chrome-gnome-shell arc-theme dmz-cursor-themes papirus-icon-theme
+
+# My setup specific.
+sudo dnf install -y solaar
