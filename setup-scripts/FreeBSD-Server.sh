127245cb517dcd664ed91b2c1772a7007b42556f
Bean6754 <Bean6754@users.noreply.github.com>
2019-04-16 00:11:07 +0100

    Update FreeBSD-Server.sh

--- setup-scripts/FreeBSD-Server.sh
+++ setup-scripts/FreeBSD-Server.sh
@@ -1,22 +1,12 @@
-#!/bin/tcsh
+#!/bin/csh
 
-# Make sure only root can run our script
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
-
-portsnap fetch
-portsnap extract
-portsnap update
-
-pkg update
-pkg upgrade -y
+su -m root -c 'pkg update'
+su -m root -c 'pkg upgrade -y'
 
 # https://www.textfixer.com/tools/alphabetize-text-words.php
 # Low-level.
-pkg install -y curl git ImageMagick neofetch p7zip rar sudo tmux unrar unzip vim-lite wget zip
+su -m root -c 'pkg install -y curl git ImageMagick neofetch p7zip rar sudo tmux unrar unzip vim-console wget zip'
 
-echo 'proc /proc procfs rw,noauto 0 0' >> /etc/fstab
+#echo 'proc /proc procfs rw,noauto 0 0' >> /etc/fstab
 
-echo 'linux_enable="YES"' >> /etc/rc.conf
+#echo 'linux_enable="YES"' >> /etc/rc.conf
