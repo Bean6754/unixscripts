e37721360ef35b70a913afb1209aece6e41ae50b
Isabella <Bean6754@users.noreply.github.com>
2023-04-14 01:18:06 +0100

    Update install.sh

--- setup-scripts/void-xfce/install.sh
+++ setup-scripts/void-xfce/install.sh
@@ -34,13 +34,11 @@ cp pkg_steam.txt pkg_steam2.txt
 sort pkg_steam2.txt > pkg_steam.txt
 rm -f pkg_steam2.txt
 
-# Install 'pkg_term.txt' first to enable repos.
 # Use '/usr/bin/cat' to not cause issues with my 'batcat' installation located at '/usr/local/bin/batcat'.
 xbps-install -S
-xbps-install $(/usr/bin/cat pkg_term.txt)
+xbps-install void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree
 xbps-install -S
-
-xbps-install $(/usr/bin/cat pkg_get_iplayer.txt pkg_kvm.txt pkg_desktop.txt pkg_hotspot.txt pkg_fonts.txt pkg_steam.txt)
+xbps-install $(/usr/bin/cat pkg_term.txt pkg_get_iplayer.txt pkg_kvm.txt pkg_desktop.txt pkg_hotspot.txt pkg_fonts.txt pkg_steam.txt)
 
 # Update font-cache.
 fc-cache -f
