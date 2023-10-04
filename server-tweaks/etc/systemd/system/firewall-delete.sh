6dfa05e1ae37e1be9c095542fe93f6f8380b7f22
Isabella <Bean6754@users.noreply.github.com>
2023-10-04 11:15:38 +0100

    Update firewall-delete.sh

--- server-tweaks/etc/systemd/system/firewall-delete.sh
+++ server-tweaks/etc/systemd/system/firewall-delete.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/bin/bash /usr/local/bin/bean6754/iptables-start.sh ; /bin/bash /usr/local/bin/bean6754/delete-sysctl.sh
+/bin/bash /usr/local/bin/bean6754/iptables-stop.sh ; /bin/bash /usr/local/bin/bean6754/delete-sysctl.sh
 systemctl disable firewall.service
