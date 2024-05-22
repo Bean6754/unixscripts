ecf2278a8f31f941a7cfa08c76b8768f1b98c3e3
Isabella <Bean6754@users.noreply.github.com>
2024-05-22 14:23:21 +0100

    Update iptables.sh

--- server-tweaks/iptables.sh
+++ server-tweaks/iptables.sh
@@ -1,5 +1,3 @@
-## This script would not be made possible if it wasn't for the articles over at "https://javapipe.com/iptables-ddos-protection", for part2 "https://www.cyberciti.biz/tips/linux-iptables-10-how-to-block-common-attack.html" and for part3 "https://www.cyberciti.biz/tips/linux-iptables-4-block-all-incoming-traffic-but-allow-ssh.html".
-
 #!/bin/bash
 
 # Make sure only root can run our script.
@@ -8,168 +6,212 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-echo "This script would not be made possible if it weren't for the articles over at 'https://javapipe.com/iptables-ddos-protection', for part2 'https://www.cyberciti.biz/tips/linux-iptables-10-how-to-block-common-attack.html' and for part3 'https://www.cyberciti.biz/tips/linux-iptables-4-block-all-incoming-traffic-but-allow-ssh.html'."
-sleep 2
-echo
+# Source: https://wiki.gentoo.org/wiki/Security_Handbook/Firewalls_and_Network_Security
+echo "Source: https://wiki.gentoo.org/wiki/Security_Handbook/Firewalls_and_Network_Security"
+IPTABLES=/sbin/iptables
+IPTABLESSAVE=/sbin/iptables-save
+IPTABLESRESTORE=/sbin/iptables-restore
+FIREWALL=/etc/firewall.rules
+DNS1=10.44.100.10
+DNS2=10.44.100.18
+#inside
+IIP=10.44.103.180
+IINTERFACE=eth0
+LOCAL_NETWORK=10.44.100.0/22
+#outside
+#OIP=217.157.156.144
+#OINTERFACE=eth1
 
-# iptables and ssh server hardening
+  # Reset entire ruleset before running script.
+  # Flush all rules.
+  $IPTABLES -Z
+  $IPTABLES -F
+  $IPTABLES -X
+  # Default ruleset.
+  $IPTABLES -P INPUT ACCEPT
+  $IPTABLES -P FORWARD ACCEPT
+  $IPTABLES -P OUTPUT ACCEPT
+  # Default tableset.
+  $IPTABLES -t nat -F
+  $IPTABLES -t mangle -F
+  # Re-flush all rules.
+  $IPTABLES -Z
+  $IPTABLES -F
+  $IPTABLES -X
 
-# Delete current ruleset.
-rm -rf /etc/iptables/rules.*
+  echo "Logging"
+  # Log iptables.
+  $IPTABLES -A INPUT -m limit --limit 5/min -j LOG --log-prefix "iptables-input: " --log-level 4
+  $IPTABLES -A FORWARD -m limit --limit 5/min -j LOG --log-prefix "iptables-forward: " --log-level 4
+  $IPTABLES -A OUTPUT -m limit --limit 5/min -j LOG --log-prefix "iptables-output: " --log-level 4
 
-# Flush all rules.
-/sbin/iptables -F
-/sbin/iptables -X
-# Default ruleset.
-#/sbin/iptables -P INPUT ACCEPT
-#/sbin/iptables -P FORWARD ACCEPT
-#/sbin/iptables -P OUTPUT ACCEPT
+  echo "Setting default rule to drop"
+  $IPTABLES -P FORWARD DROP
+  $IPTABLES -P INPUT   DROP
+  $IPTABLES -P OUTPUT  DROP
 
-# Log iptables.
-/sbin/iptables -A INPUT -m limit --limit 5/min -j LOG --log-prefix "iptables-input: " --log-level 4
-/sbin/iptables -A FORWARD -m limit --limit 5/min -j LOG --log-prefix "iptables-forward: " --log-level 4
-/sbin/iptables -A OUTPUT -m limit --limit 5/min -j LOG --log-prefix "iptables-output: " --log-level 4
+  #default rule
+  echo "Creating states chain"
+  $IPTABLES -N allowed-connection
+  $IPTABLES -F allowed-connection
+  $IPTABLES -A allowed-connection -m state --state ESTABLISHED,RELATED -j ACCEPT
+  $IPTABLES -A allowed-connection -i $IINTERFACE -m limit -j LOG --log-prefix \
+      "Bad packet from ${IINTERFACE}:"
+  $IPTABLES -A allowed-connection -j DROP
 
-echo "Part 1."
+  #ICMP traffic
+  echo "Creating icmp chain"
+  $IPTABLES -N icmp_allowed
+  $IPTABLES -F icmp_allowed
+  $IPTABLES -A icmp_allowed -m state --state NEW -p icmp --icmp-type \
+      time-exceeded -j ACCEPT
+  $IPTABLES -A icmp_allowed -m state --state NEW -p icmp --icmp-type \
+      destination-unreachable -j ACCEPT
+  $IPTABLES -A icmp_allowed -p icmp -j LOG --log-prefix "Bad ICMP traffic:"
+  $IPTABLES -A icmp_allowed -p icmp -j DROP
 
-/sbin/iptables -t mangle -A PREROUTING -m conntrack --ctstate INVALID -j DROP
-echo "Added rule: 1. Drop invalid packets."
-echo
+  #Incoming traffic
+  echo "Creating incoming ssh traffic chain"
+  ## SSH.
+  $IPTABLES -N allow-ssh-traffic-in
+  $IPTABLES -F allow-ssh-traffic-in
+  #Flood protection
+  $IPTABLES -A allow-ssh-traffic-in -m limit --limit 1/second -p tcp --tcp-flags \
+      ALL RST --dport ssh -j ACCEPT
+  $IPTABLES -A allow-ssh-traffic-in -m limit --limit 1/second -p tcp --tcp-flags \
+      ALL FIN --dport ssh -j ACCEPT
+  $IPTABLES -A allow-ssh-traffic-in -m limit --limit 1/second -p tcp --tcp-flags \
+      ALL SYN --dport ssh -j ACCEPT
+  $IPTABLES -A allow-ssh-traffic-in -m state --state RELATED,ESTABLISHED -p tcp --dport ssh -j ACCEPT
+  ## Samba.
+  $IPTABLES -N allow-samba-traffic-in
+  $IPTABLES -F allow-samba-traffic-in
+  # Flood protection.
+  # TCP.
+  $IPTABLES -A allow-samba-traffic-in -m limit --limit 1/second -p tcp --tcp-flags \
+      ALL RST -m multiport --dports 139,445 -j ACCEPT
+  $IPTABLES -A allow-samba-traffic-in -m limit --limit 1/second -p tcp --tcp-flags \
+      ALL FIN -m multiport --dports 139,445 -j ACCEPT
+  $IPTABLES -A allow-samba-traffic-in -m limit --limit 1/second -p tcp --tcp-flags \
+      ALL SYN -m multiport --dports 139,445 -j ACCEPT
+  $IPTABLES -A allow-samba-traffic-in -m state --state RELATED,ESTABLISHED -p tcp -m multiport --dports 139,445 -j ACCEPT
+  # UDP.
+  $IPTABLES -A allow-samba-traffic-in -m limit --limit 1/second -p udp -m multiport --dports 137,138 -j ACCEPT
+  #$IPTABLES -A allow-samba-traffic-in -m state --state RELATED,ESTABLISHED -p udp -m multiport --dports 137,138 -j ACCEPT
+  ## SMTP.
+  $IPTABLES -N allow-smtp-traffic-in
+  $IPTABLES -F allow-smtp-traffic-in
+  # Flood protection.
+  # TCP.
+  $IPTABLES -A allow-smtp-traffic-in -m limit --limit 1/second -p tcp --tcp-flags \
+      ALL RST --dport 25 -j ACCEPT
+  $IPTABLES -A allow-smtp-traffic-in -m limit --limit 1/second -p tcp --tcp-flags \
+      ALL FIN --dport 25 -j ACCEPT
+  $IPTABLES -A allow-smtp-traffic-in -m limit --limit 1/second -p tcp --tcp-flags \
+      ALL SYN --dport 25 -j ACCEPT
+  $IPTABLES -A allow-smtp-traffic-in -m state --state RELATED,ESTABLISHED -p tcp --dport 25 -j ACCEPT
+  ## Syslog.
+  $IPTABLES -N allow-syslog-traffic-in
+  $IPTABLES -F allow-syslog-traffic-in
+  # Flood protection.
+  # TCP.
+  $IPTABLES -A allow-syslog-traffic-in -m limit --limit 1/second -p tcp --tcp-flags \
+      ALL RST --dport 514 -j ACCEPT
+  $IPTABLES -A allow-syslog-traffic-in -m limit --limit 1/second -p tcp --tcp-flags \
+      ALL FIN --dport 514 -j ACCEPT
+  $IPTABLES -A allow-syslog-traffic-in -m limit --limit 1/second -p tcp --tcp-flags \
+      ALL SYN --dport 514 -j ACCEPT
+  $IPTABLES -A allow-syslog-traffic-in -m state --state RELATED,ESTABLISHED -p tcp --dport 514 -j ACCEPT
+  # UDP.
+  $IPTABLES -A allow-syslog-traffic-in -m limit --limit 1/second -p udp --dport 514 -j ACCEPT
+  #$IPTABLES -A allow-syslog-traffic-in -m state --state RELATED,ESTABLISHED -p udp --dport 514 -j ACCEPT
+  ## Web.
+  $IPTABLES -N allow-www-traffic-in
+  $IPTABLES -F allow-www-traffic-in
+  # Flood protection.
+  $IPTABLES -A allow-www-traffic-in -m limit --limit 1/second -p tcp --tcp-flags \
+      ALL RST -m multiport --dports 80,443 -j ACCEPT
+  $IPTABLES -A allow-www-traffic-in -m limit --limit 1/second -p tcp --tcp-flags \
+      ALL FIN -m multiport --dports 80,443 -j ACCEPT
+  $IPTABLES -A allow-www-traffic-in -m limit --limit 1/second -p tcp --tcp-flags \
+      ALL SYN -m multiport --dports 80,443 -j ACCEPT
+  $IPTABLES -A allow-www-traffic-in -m state --state RELATED,ESTABLISHED -p tcp -m multiport --dports 80,443 -j ACCEPT
 
-/sbin/iptables -t mangle -A PREROUTING -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
-echo "Added rule: 2. Drop TCP packets that are new and are not SYN."
-echo
+  #outgoing traffic
+  echo "Creating outgoing ssh traffic chain"
+  $IPTABLES -N allow-ssh-traffic-out
+  $IPTABLES -F allow-ssh-traffic-out
+  $IPTABLES -A allow-ssh-traffic-out -p tcp --dport ssh -j ACCEPT
 
-/sbin/iptables -t mangle -A PREROUTING -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -j DROP
-echo "Added rule: 3. Drop SYN packets with suspicious MSS value."
-echo
+  echo "Creating outgoing dns traffic chain"
+  $IPTABLES -N allow-dns-traffic-out
+  $IPTABLES -F allow-dns-traffic-out
+  $IPTABLES -A allow-dns-traffic-out -p udp -d $DNS1 --dport domain \
+      -j ACCEPT
+  $IPTABLES -A allow-dns-traffic-out -p udp -d $DNS2 --dport domain \
+     -j ACCEPT
 
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,ACK FIN -j DROP
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,FIN FIN -j DROP
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL ALL -j DROP
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL NONE -j DROP
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP
-/sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
-echo "Added rule: 4. Block packets with bogus TCP flags."
-echo
+  echo "Creating outgoing http/https traffic chain"
+  $IPTABLES -N allow-www-traffic-out
+  $IPTABLES -F allow-www-traffic-out
+  $IPTABLES -A allow-www-traffic-out -p tcp --dport www -j ACCEPT
+  $IPTABLES -A allow-www-traffic-out -p tcp --dport https -j ACCEPT
 
-### BREAKS LOCAL-NAT (VMs and Containers) AND WIFI ON LAPTOPS!
-# IMPORTANT: Replace your network interface, ip address and ip range here!
-#/sbin/iptables -A INPUT -i eth0 -s 172.16.1.10 -j DROP
-#/sbin/iptables -t mangle -A PREROUTING -s 224.0.0.0/3 -j DROP
-#/sbin/iptables -t mangle -A PREROUTING -s 169.254.0.0/16 -j DROP
-#/sbin/iptables -t mangle -A PREROUTING -s 172.16.0.0/12 -j DROP
-#/sbin/iptables -t mangle -A PREROUTING -s 192.0.2.0/24 -j DROP
-#/sbin/iptables -t mangle -A PREROUTING -s 192.168.0.0/16 -j DROP
-#/sbin/iptables -t mangle -A PREROUTING -s 10.0.0.0/8 -j DROP
-#/sbin/iptables -t mangle -A PREROUTING -s 0.0.0.0/8 -j DROP
-#/sbin/iptables -t mangle -A PREROUTING -s 240.0.0.0/5 -j DROP
-#/sbin/iptables -t mangle -A PREROUTING -s 127.0.0.0/8 ! -i lo -j DROP
-echo "Added rule: 5. Block spoofed packets."
-echo
+  #Catch portscanners
+  echo "Creating portscan detection chain"
+  $IPTABLES -N check-flags
+  $IPTABLES -F check-flags
+  $IPTABLES -A check-flags -p tcp --tcp-flags ALL FIN,URG,PSH -m limit \
+      --limit 5/minute -j LOG --log-level alert --log-prefix "NMAP-XMAS:"
+  $IPTABLES -A check-flags -p tcp --tcp-flags ALL FIN,URG,PSH -j DROP
+  $IPTABLES -A check-flags -p tcp --tcp-flags ALL ALL -m limit --limit \
+      5/minute -j LOG --log-level 1 --log-prefix "XMAS:"
+  $IPTABLES -A check-flags -p tcp --tcp-flags ALL ALL -j DROP
+  $IPTABLES -A check-flags -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG \
+      -m limit --limit 5/minute -j LOG --log-level 1 --log-prefix "XMAS-PSH:"
+  $IPTABLES -A check-flags -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
+  $IPTABLES -A check-flags -p tcp --tcp-flags ALL NONE -m limit \
+      --limit 5/minute -j LOG --log-level 1 --log-prefix "NULL_SCAN:"
+  $IPTABLES -A check-flags -p tcp --tcp-flags ALL NONE -j DROP
+  $IPTABLES -A check-flags -p tcp --tcp-flags SYN,RST SYN,RST -m limit \
+      --limit 5/minute -j LOG --log-level 5 --log-prefix "SYN/RST:"
+  $IPTABLES -A check-flags -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
+  $IPTABLES -A check-flags -p tcp --tcp-flags SYN,FIN SYN,FIN -m limit \
+      --limit 5/minute -j LOG --log-level 5 --log-prefix "SYN/FIN:"
+  $IPTABLES -A check-flags -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP
 
-#/sbin/iptables -t mangle -A PREROUTING -p icmp -j DROP
-/sbin/iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 1/s --limit-burst 2 -j ACCEPT
-/sbin/iptables -A INPUT -p icmp -j DROP
-echo "Added rule: 6. Limit ICMP. You usually do not need this protocol."
-echo
+  # Apply and add invalid states to the chains
+  echo "Applying chains to INPUT"
+  $IPTABLES -A INPUT -m state --state INVALID -j DROP
+  $IPTABLES -A INPUT -p icmp -j icmp_allowed
+  $IPTABLES -A INPUT -j check-flags
+  $IPTABLES -A INPUT -i lo -j ACCEPT
+  $IPTABLES -A INPUT -j allow-ssh-traffic-in
+  $IPTABLES -A INPUT -j allow-www-traffic-in
+  $IPTABLES -A INPUT -j allow-samba-traffic-in
+  $IPTABLES -A INPUT -j allow-smtp-traffic-in
+  $IPTABLES -A INPUT -j allow-syslog-traffic-in
+  $IPTABLES -A INPUT -j allowed-connection
 
-/sbin/iptables -t mangle -A PREROUTING -f -j DROP
-echo "Added rule: 7. Drop fragments in all chains."
-echo
+  echo "Applying chains to FORWARD"
+  $IPTABLES -A FORWARD -m state --state INVALID -j DROP
+  $IPTABLES -A FORWARD -p icmp -j icmp_allowed
+  $IPTABLES -A FORWARD -j check-flags
+  $IPTABLES -A FORWARD -o lo -j ACCEPT
+  $IPTABLES -A FORWARD -j allow-ssh-traffic-in
+  $IPTABLES -A FORWARD -j allow-www-traffic-out
+  $IPTABLES -A FORWARD -j allowed-connection
 
-/sbin/iptables -A INPUT -p tcp -m connlimit --connlimit-above 111 -j REJECT --reject-with tcp-reset
-echo "Added rule: 8. Limit connections per source IP."
-echo
+  echo "Applying chains to OUTPUT"
+  $IPTABLES -A OUTPUT -m state --state INVALID -j DROP
+  $IPTABLES -A OUTPUT -p icmp -j icmp_allowed
+  $IPTABLES -A OUTPUT -j check-flags
+  $IPTABLES -A OUTPUT -o lo -j ACCEPT
+  $IPTABLES -A OUTPUT -j allow-ssh-traffic-out
+  $IPTABLES -A OUTPUT -j allow-dns-traffic-out
+  $IPTABLES -A OUTPUT -j allow-www-traffic-out
+  $IPTABLES -A OUTPUT -j allowed-connection
 
-/sbin/iptables -A INPUT -p tcp --tcp-flags RST RST -m limit --limit 2/s --limit-burst 2 -j ACCEPT
-/sbin/iptables -A INPUT -p tcp --tcp-flags RST RST -j DROP
-echo "Added rule: 9. Limit RST packets."
-echo
-
-/sbin/iptables -A INPUT -p tcp -m conntrack --ctstate NEW -m limit --limit 60/s --limit-burst 20 -j ACCEPT
-/sbin/iptables -A INPUT -p tcp -m conntrack --ctstate NEW -j DROP
-echo "Added rule: 10. Limit new TCP connections per second per source IP."
-echo
-
-# For some reason this blocks the ability to port forward.
-# /sbin/iptables -t raw -A PREROUTING -p tcp -m tcp --syn -j CT --notrack
-/sbin/iptables -A INPUT -p tcp -m tcp -m conntrack --ctstate INVALID,UNTRACKED -j SYNPROXY --sack-perm --timestamp --wscale 7 --mss 1460
-/sbin/iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
-echo "Added rule: 11. Use SYNPROXY on all ports. Disables connection limiting rule."
-echo
-
-# Extras.
-/sbin/iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --set
-/sbin/iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 10 -j DROP
-echo "Added rule: Extra. SSH brute-force protection."
-echo
-
-/sbin/iptables -N port-scanning
-/sbin/iptables -A port-scanning -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s --limit-burst 2 -j RETURN
-/sbin/iptables -A port-scanning -j DROP
-echo "Added rule: Extra. Protection against port scanning."
-echo
-
-echo "Done part1."
-
-echo 
-
-echo "Now for part 2."
-# Part 2. "https://www.cyberciti.biz/tips/linux-iptables-10-how-to-block-common-attack.html"
-/sbin/iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
-echo "Added rule 1. Force SYN packets check."
-echo
-
-/sbin/iptables -A INPUT -f -j DROP
-echo "Added rule 2. Force Fragments packets check."
-echo
-
-/sbin/iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
-echo "Added rule 3. Drop incoming malformed XMAS packets."
-echo
-
-/sbin/iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
-echo "Added rule 3. Drop incoming malformed NULL packets."
-echo
-
-echo "Done part 2."
-
-echo "Now for part 3."
-# https://www.cyberciti.biz/tips/linux-iptables-4-block-all-incoming-traffic-but-allow-ssh.html
-# dport = destination port
-# sport = source port
-#iptables -A INPUT -p tcp -s 0/0 -d $SERVER_IP --sport 513:65535 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
-#iptables -A OUTPUT -p tcp -s $SERVER_IP -d 0/0 --sport 22 --dport 513:65535 -m state --state ESTABLISHED -j ACCEPT
-# SSH (TCP).
-/sbin/iptables -A INPUT -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
-# HTTP and HTTPS (TCP).
-/sbin/iptables -A INPUT -p tcp -m multiport --dports 80,443 -m state --state NEW,ESTABLISHED -j ACCEPT
-
-/sbin/iptables -A INPUT -i lo -j ACCEPT
-/sbin/iptables -A OUTPUT -o lo -j ACCEPT
-
-# Set 'INPUT' to 'ACCEPT' instead of 'DROP' and enable FORWARD to 'ACCEPT' if you are setting-up a router.
-/sbin/iptables -A INPUT -j DROP
-#/sbin/iptables -A FORWARD -j ACCEPT
-/sbin/iptables -A OUTPUT -j ACCEPT
-
-# Enable apt.
-/sbin/iptables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-
-echo "Done part 3."
-echo "Done altogether."
-
-echo "Saving rules.."
-mkdir -p /etc/iptables
-/sbin/iptables-save > /etc/iptables/rules.v4
-/sbin/ip6tables-save > /etc/iptables/rules.v6
-echo "Rules saved."
+  #Allow client to route through via NAT (Network Address Translation)
+  # For router.
+  #$IPTABLES -t nat -A POSTROUTING -o $OINTERFACE -j MASQUERADE
